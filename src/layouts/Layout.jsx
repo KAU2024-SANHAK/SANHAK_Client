eb48f81be3b819cc20e3539546e29a446056c725
Wang HoEun <hoeunwang@gmail.com>
2024-03-23 23:37:01 +0900

    feat: Layout 컴포넌트 생성 후 outlet 적용 + useMobile hooks 적용

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,14 @@
+import { useMobile } from "../hooks/useMobile";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  useMobile();
+
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
