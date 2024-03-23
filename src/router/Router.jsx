d7537121d2191138c39cf460173d9b413d8cd26e
Wang HoEun <hoeunwang@gmail.com>
2024-03-23 23:37:22 +0900

    feat: router 폴더 및 파일 생성 후 예시 코드 작성

---
+++ src/router/Router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layouts/Layout";
+
+/*이런 형식으로 작성하도록 합니다.*/
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      // {
+      //   path: "/",
+      //   element: <Start />,
+      // },
+      // {
+      //   path: "/api/oauth/kakao/login",
+      //   element: <Login />,
+      // },
+    ],
+  },
+]);
+
+export default router;
