c9d3c7414f79389c332e47a625bd29365c32eb1a
Wang HoEun <hoeunwang@gmail.com>
2024-03-25 17:52:36 +0900

    feat: react-query 컴포넌트 적용 및 errorboundary 적용 + suspense 활용 로딩 ui 도출 되게끔 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react';
-import './App.css';
+import { Suspense } from 'react';
+import Loading from './pages/Loading/Loading';
+import { ErrorBoundary } from 'react-error-boundary';
+import { QueryClientProvider, QueryErrorResetBoundary } from 'react-query';
+import FallbackUI from './pages/FallbackUI/FallbackUI';
+import * as S from './App.style';
 
 function App() {
-  return function App() {
-    return (
-      <>
-        <RecoilRoot>
-          <RouterProvider router={router} />
-        </RecoilRoot>
-      </>
-    );
-  };
+  const queryClient = new QueryClient();
+  return (
+    <S.Wrapper>
+      <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <QueryErrorResetBoundary>
+            {({ reset }) => (
+              <ErrorBoundary onReset={() => reset()} FallbackComponent={FallbackUI}>
+                <Suspense fallback={<Loading />}>
+                  <RouterProvider router={router} />
+                </Suspense>
+              </ErrorBoundary>
+            )}
+          </QueryErrorResetBoundary>
+        </QueryClientProvider>
+      </RecoilRoot>
+    </S.Wrapper>
+  );
 }
 
 export default App;
