a407f15ac04e82ea92e2a5c84607579b42bc8bf2
Wang HoEun <hoeunwang@gmail.com>
2024-03-25 17:50:56 +0900

    feat: react-query 폴더 및 파일 생성 후 예시 코드 작성

---
+++ src/hooks/queries/member/useGetMypage.jsx
@@ -0,0 +1,18 @@
+/*이렇게 코드 작성 !! get method 코드*/
+
+// import { useSuspenseQuery } from '@tanstack/react-query';
+// import { get } from '../../../apis/client';
+
+// export const MYPAGE_BY_ID_QUERY_KEY= ['myPageData'];
+
+// export const fetchMyPage = async () => get(`/member/mypage`);
+
+// const useGetMyPage = () => {
+//   const { data } = useSuspenseQuery({
+//     queryKey: MYPAGE_BY_ID_QUERY_KEY,
+//     queryFn: () => fetchMyPage(),
+//   });
+//   return data;
+// };
+
+// export default useGetMyPage;
