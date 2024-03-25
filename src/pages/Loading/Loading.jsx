0c9b8b54d20a7e0d37f5536f6a27b8770b52f351
Wang HoEun <hoeunwang@gmail.com>
2024-03-25 17:51:11 +0900

    feat: 임시 loading 페이지 생성

---
+++ src/pages/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import * as S from './Loading.style';
+
+const Loading = () => {
+  return (
+    <S.LoadingWrapper>
+      <S.Text>불러오는 중...</S.Text>
+    </S.LoadingWrapper>
+  );
+};
+
+export default Loading;
