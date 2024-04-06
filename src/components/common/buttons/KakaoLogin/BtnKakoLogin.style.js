b09cc27f6d31e9cf10c483ebe52c77fa2780498e
P1su <pissssssu@naver.com>
2024-04-06 20:07:21 +0900

    style: 카카오 로그인 버튼 위치 수정

--- src/components/common/buttons/KakaoLogin/BtnKakoLogin.style.js
+++ src/components/common/buttons/KakaoLogin/BtnKakoLogin.style.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const Wrapper = styled.button`   
     ${({ theme: { mixin } }) => mixin.flexCenter({})}  
-    
+    position: relative;
+    top: 20rem;
     border: none;
     background-color: transparent; 
 `;
