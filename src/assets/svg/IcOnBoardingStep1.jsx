0c28e7f08e241cbc77406ef9f96b884298aa2c9d
P1su <pissssssu@naver.com>
2024-04-08 23:01:47 +0900

    feat: NxtBtn, Honeybear svg 파일추가

---
+++ src/assets/svg/IcOnBoardingStep1.jsx
@@ -0,0 +1,5 @@
+export default function IcOnBoardingStep1(){
+    return(
+        <></>
+    )
+}
---
+++ src/assets/svg/IcOnBoardingStep4.jsx
@@ -0,0 +1,5 @@
+export default function IcOnBoardingStep4(){
+    return(
+        <></>
+    )
+}
