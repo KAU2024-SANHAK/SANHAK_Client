8b3b7c613ac20c543aac7cca7bbf94b15fb8ef2b
P1su <pissssssu@naver.com>
2024-04-08 14:34:58 +0900

    chore: ProgreesBar 및 BtnNext 변수명 변경(OnBoarding 추가)

--- src/components/OnBoardingProgressBar/OnBoardingPrgoressBar.jsx
+++ src/components/OnBoardingProgressBar/OnBoardingPrgoressBar.jsx
@@ -1,4 +1,4 @@
-import * as S from "./ProgressBar.style"
+import * as S from "./OnBoardingProgressBar.style"
 import SvgIcProgressCircle from "../../assets/svg/IcProgressCircle"
 import SvgIcCurrentProgressCircle from "../../assets/svg/IcCurrentProgressCircle"
 export default function ProgressBar(props){
