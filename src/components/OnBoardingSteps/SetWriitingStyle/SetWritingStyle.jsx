3bb1ecf4c2788bc12d91fbd9a8fac8b14b5058de
P1su <pissssssu@naver.com>
2024-04-08 19:52:36 +0900

    code review: hadndleSetWritingStyle 함수에 console.log() 작성

--- src/components/OnBoardingSteps/SetWriitingStyle/SetWritingStyle.jsx
+++ src/components/OnBoardingSteps/SetWriitingStyle/SetWritingStyle.jsx
@@ -4,6 +4,7 @@ const handleSetWritingStyle= ()=>{
     //선호하는 스타일 전달
     //axios post 이용하면 되겠져>>?? instance 파둬야겠네.////.......
     //이건 ai로 보내나요 백으로 보내나요? 
+    console.log(alert('api 요청하세요'))
 }
 export default function SetWritingStyle(){
     return(
