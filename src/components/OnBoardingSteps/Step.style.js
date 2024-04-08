869f07af98effc55a1ae32a2bf130dfdc3330751
P1su <pissssssu@naver.com>
2024-04-08 19:45:08 +0900

    code review: Step.style mixin 사용하여 flex 정리

--- src/components/OnBoardingSteps/Step.style.js
+++ src/components/OnBoardingSteps/Step.style.js
@@ -5,20 +5,15 @@ export const Wrapper = styled.div`
     ${({ theme: { mixin } }) => mixin.flexCenter({})};
 `
 export const Body = styled.div`
-    display: flex;
+    ${({ theme: { mixin } }) => mixin.flexBox({direction:'column', align: 'center' })};
     width: 100%;
-    flex-direction: column;
-    align-items: center;
-    hegiht: 21.25rem;
 `
 export const Footer = styled.div`
-    display: flex;
-    flex-direction: column;
+    ${({ theme: { mixin } }) => mixin.flexBox({direction:'column', align: 'center' })};
     position: absolute;
     bottom: 7rem;
     left: 50%;
     transform: translateX(-50%);
-    align-items: center;
 
 `
 
