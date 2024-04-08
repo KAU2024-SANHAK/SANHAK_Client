4d189de8ae893fa09df4717cf470f2558891cfc3
P1su <pissssssu@naver.com>
2024-04-08 22:21:24 +0900

    style: 배경 및 OnboardingProgressBar 색상 변경

--- src/pages/OnBoarding/OnBoarding.style.js
+++ src/pages/OnBoarding/OnBoarding.style.js
@@ -5,5 +5,6 @@ export const Wrapper = styled.div`
     align-items: center;
     width: 100%;
     height: 100vh;
+    background: linear-gradient(187deg, #D9ACB7 20.43%, #DCC6CC 100.45%, #999 100.45%);
 
 `
