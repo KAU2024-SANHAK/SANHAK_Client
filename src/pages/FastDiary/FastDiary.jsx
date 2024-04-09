4749b123f1abd0bbae466c3c7ef7f36934136a25
P1su <pissssssu@naver.com>
2024-04-10 00:04:33 +0900

    feat: form 작성 styled-component 배치

--- src/pages/FastDiary/FastDiary.jsx
+++ src/pages/FastDiary/FastDiary.jsx
@@ -4,10 +4,11 @@ export default function FastDiary(){
     const date = new Date();
     const today = date.getDate();
     console.log(today%2)
-
     return(
         <S.FastDairyPageWrapper $isEven = {today%2}>
-            <h1>diary</h1>
+            <S.WritingForm>
+
+            </S.WritingForm>
         </S.FastDairyPageWrapper>
     )
 }
--- src/pages/FastDiary/FastDiary.style.js
+++ src/pages/FastDiary/FastDiary.style.js
@@ -1,9 +1,19 @@
 import styled from 'styled-components'
-
+//background: linear-gradient(187deg, #FFE768 20.43%, #FFF3B7 84.05%);
+//linear-gradient(187deg, #FFBBCB 20.43%, #FFDAE3 81.25%, #DCC6CC 100.45%, #999 100.45%)
 export const FastDairyPageWrapper = styled.div`
     width: 100%;
     height: 100vh;
     background: ${(props) => (props.isEven === 0 ? 'linear-gradient(187deg, #FFBBCB 20.43%, #FFDAE3 81.25%, #DCC6CC 100.45%, #999 100.45%)'
-    :'linear-gradient(187deg, #FFE768 20.43%, #FFF3B7 84.05%, #999 100.45%)'
+    : 'linear-gradient(187deg, #FFE768 20.43%, #FFF3B7 84.05%)'
     )};
+`
+export const WritingForm = styled.form`
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 431px;
+    border-radius: 30px;
+    background: rgba(255, 255, 255, 0.50);
+    backdrop-filter: blur(15px);
 `
