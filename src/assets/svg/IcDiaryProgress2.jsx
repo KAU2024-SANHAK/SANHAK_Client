06b65c73b272c577dca0faeed8b585470f3fffa0
P1su <pissssssu@naver.com>
2024-04-10 00:41:58 +0900

    chore: diary progress svg 파일 camelCase로 수정

--- src/assets/svg/IcDiaryProgress2.jsx
+++ src/assets/svg/IcDiaryProgress2.jsx
@@ -1,7 +1,7 @@
 export default function SvgIcDiaryProgress2(){
     return(
         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="184" viewBox="0 0 50 184" fill="none">
-            <rect width="50" height="184" rx="25" fill="white" fill-opacity="0.2"/>
+            <rect width="50" height="184" rx="25" fill="white" fillOpacity="0.2"/>
             <rect y="114" width="50" height="70" rx="25" fill="#FFBF60"/>
         </svg>
     )
--- src/assets/svg/IcDiaryProgress6.jsx
+++ src/assets/svg/IcDiaryProgress6.jsx
@@ -1,7 +1,7 @@
 export default function SvgIcDiaryProgress6(){
     return(
         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="184" viewBox="0 0 50 184" fill="none">
-            <rect width="50" height="184" rx="25" fill="white" fill-opacity="0.2"/>
+            <rect width="50" height="184" rx="25" fill="white" fillOpacity="0.2"/>
             <rect y="34" width="50" height="150" rx="25" fill="#FFBF60"/>
         </svg>
     )
