54ef2fd510824f3215db8b575c21d073d613dca0
P1su <pissssssu@naver.com>
2024-04-09 20:37:12 +0900

    chore: NextButton svg 파일 camelCase 수정

--- src/assets/svg/IcNextButton.jsx
+++ src/assets/svg/IcNextButton.jsx
@@ -2,7 +2,7 @@ export default function SvgIcNextButton(){
     return(
         <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
             <rect width="60" height="60" rx="30" fill="white"/>
-            <path d="M27 39L35.2929 30.7071C35.6834 30.3166 35.6834 29.6834 35.2929 29.2929L27 21" stroke="black" stroke-width="4" stroke-linecap="round"/>
+            <path d="M27 39L35.2929 30.7071C35.6834 30.3166 35.6834 29.6834 35.2929 29.2929L27 21" stroke="black" strokeWidth="4" strokeLinecap="round"/>
         </svg>
     )
 }
