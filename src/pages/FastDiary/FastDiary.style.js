41eba533d49b33f9e0426281ea3f036ed8607483
P1su <pissssssu@naver.com>
2024-04-10 14:34:37 +0900

    chore: FastDiary.style 주석 삭제 및 오타 수정

--- src/pages/FastDiary/FastDiary.style.js
+++ src/pages/FastDiary/FastDiary.style.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
 import { HomeButton } from '../../components/common/buttons/Home/BtnHome.style'
-//background: linear-gradient(187deg, #FFE768 20.43%, #FFF3B7 84.05%);
-//linear-gradient(187deg, #FFBBCB 20.43%, #FFDAE3 81.25%, #DCC6CC 100.45%, #999 100.45%)
+
 export const FastDairyPageWrapper = styled.div`
     ${({ theme: { mixin } }) => mixin.flexCenter({})};
     width: 100%;
     height: 100vh;
-    background: ${(props) => (props.isEven === 0 ? 'linear-gradient(187deg, #FFBBCB 20.43%, #FFDAE3 81.25%, #DCC6CC 100.45%, #999 100.45%)'
+    background: ${(props) => (props.$isEven === 0 ? 'linear-gradient(187deg, #FFBBCB 20.43%, #FFDAE3 81.25%, #DCC6CC 100.45%, #999 100.45%)'
     : 'linear-gradient(187deg, #FFE768 20.43%, #FFF3B7 84.05%)'
     )};
 `
@@ -14,7 +13,6 @@ export const Header = styled.div`
     ${({ theme: { mixin } }) => mixin.flexBox({justify: 'space-between',align: 'center' })};
     width: 100%;
     margin-top: 3rem;
-
 
 `
 export const WritingForm = styled.form`
