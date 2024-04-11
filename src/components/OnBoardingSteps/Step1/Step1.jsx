d9c0b7799edfe4b2baddfd8abed621d61af35dbb
P1su <pissssssu@naver.com>
2024-04-11 17:58:44 +0900

    feat: png로 Step1 이미지 불러오기

--- src/components/OnBoardingSteps/Step1/Step1.jsx
+++ src/components/OnBoardingSteps/Step1/Step1.jsx
@@ -4,6 +4,7 @@ import * as S from "../Step.style"
 import Title from "../title/Title";
 import SubTitle from "../title/SubTitle";
 
+import sample from '../../../assets/sample.png'
 //온보딩 최적화 부분이 조금 어려운 것 같아요...
 //여기서 현재 상태랑 전체 배열도 넘겨줘야 된다 생각하는데
 //지저분할 것 같고, 또 그렇다면 내부 구현 변수는 어떻게 처리하는지,,,ㅠㅠ
@@ -27,6 +28,7 @@ export default function Step1(props){
             </SubTitle>
 
             <S.Body>
+                <img src={sample}/>
                 <S.BlankImage />
             </S.Body>
             
