d6e4d783fdf1df557e753b30303814692c48d5d2
P1su <pissssssu@naver.com>
2024-04-12 00:56:40 +0900

    code review: DiaryProgress.jsx에서 svg 파일 호출하도록 수정

--- src/components/DiaryProgress/DiaryProgress.jsx
+++ src/components/DiaryProgress/DiaryProgress.jsx
@@ -1,14 +1,20 @@
 import * as S from './DiaryProgress.style'
+import SvgIcDiaryProgress1 from "../../assets/svg/IcDiaryProgress1"
+import SvgIcDiaryProgress2 from "../../assets/svg/IcDiaryProgress2"
+import SvgIcDiaryProgress3 from "../../assets/svg/IcDiaryProgress3"
+import SvgIcDiaryProgress4 from "../../assets/svg/IcDiaryProgress4"
+import SvgIcDiaryProgress5 from "../../assets/svg/IcDiaryProgress5"
+import SvgIcDiaryProgress6 from "../../assets/svg/IcDiaryProgress6"
 
 const Progresses = [
-    <S.DiaryProgress1/>,
-    <S.DiaryProgress2/>,
-    <S.DiaryProgress3/>,
-    <S.DiaryProgress4/>,
-    <S.DiaryProgress5/>,
-    <S.DiaryProgress6/>
+    <SvgIcDiaryProgress1/>,
+    <SvgIcDiaryProgress2/>,
+    <SvgIcDiaryProgress3/>,
+    <SvgIcDiaryProgress4/>,
+    <SvgIcDiaryProgress5/>,
+    <SvgIcDiaryProgress6/>
 ]
-//이 부분 별로 안좋은 구현인 것 같아요 ㅠㅠ....
+
 export default function DiaryPrgoress(props){
     
     return(
--- src/components/DiaryProgress/DiaryProgress.style.js
+++ src/components/DiaryProgress/DiaryProgress.style.js
@@ -1,29 +1,5 @@
 import styled from 'styled-components'
-import SvgIcDiaryProgress1 from "../../assets/svg/IcDiaryProgress1"
-import SvgIcDiaryProgress2 from "../../assets/svg/IcDiaryProgress2"
-import SvgIcDiaryProgress3 from "../../assets/svg/IcDiaryProgress3"
-import SvgIcDiaryProgress4 from "../../assets/svg/IcDiaryProgress4"
-import SvgIcDiaryProgress5 from "../../assets/svg/IcDiaryProgress5"
-import SvgIcDiaryProgress6 from "../../assets/svg/IcDiaryProgress6"
 
 export const Wrapper = styled.div`
     margin-right: 3rem;
-`
-export const DiaryProgress1 = styled(SvgIcDiaryProgress1)`
-
-`
-export const DiaryProgress2 = styled(SvgIcDiaryProgress2)`
-
-`
-export const DiaryProgress3 = styled(SvgIcDiaryProgress3)`
-
-`
-export const DiaryProgress4 = styled(SvgIcDiaryProgress4)`
-
-`
-export const DiaryProgress5 = styled(SvgIcDiaryProgress5)`
-
-`
-export const DiaryProgress6 = styled(SvgIcDiaryProgress6)`
-
 `
