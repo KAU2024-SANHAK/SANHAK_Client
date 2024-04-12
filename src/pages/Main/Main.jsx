ab9e8ad22e9795b0add5830b465e0dc601f86824
funcsom <bishop@kau.kr>
2024-04-12 20:00:00 +0900

    feat: main페이지에 마이페이지 버튼 설정!

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,8 +4,7 @@ import MyPageButton from '../../components/common/buttons/MyPageButton/MyPageBut
 export default function Main(){
     return(
         <S.Wrapper>
-            <h1>메인 화면입니다.</h1>
-            
+            <MyPageButton/>         
         </S.Wrapper>
     )
 }
