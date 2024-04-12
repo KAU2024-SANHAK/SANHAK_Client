50268598f71f21b2b8b4a5e0f19735a7b737e6b6
funcsom <bishop@kau.kr>
2024-04-13 01:50:38 +0900

    feat: MyPageButton 글 없이 커밋

--- src/assets/svg/IcMyPageButton.jsx
+++ src/assets/svg/IcMyPageButton.jsx
@@ -1,14 +1,15 @@
 const SvgIcMyPageButton=()=>(
     <svg width="93" height="119" viewBox="0 0 93 119" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g filter="url(#filter0_b_464_2280)">
+        <g filter="url(#filter0_b_710_286)">
             <rect width="93" height="119" rx="12" fill="white" fill-opacity="0.27"/>
         </g>
+        <path d="M46.5 20C31.872 20 20 31.872 20 46.5C20 61.128 31.872 73 46.5 73C61.128 73 73 61.128 73 46.5C73 31.872 61.128 20 46.5 20ZM46.5 30.6C51.6145 30.6 55.775 34.7605 55.775 39.875C55.775 44.9895 51.6145 49.15 46.5 49.15C41.3855 49.15 37.225 44.9895 37.225 39.875C37.225 34.7605 41.3855 30.6 46.5 30.6ZM46.5 67.7C41.1205 67.7 34.7605 65.527 30.229 60.068C34.7075 56.57 40.352 54.45 46.5 54.45C52.648 54.45 58.2925 56.57 62.771 60.068C58.2395 65.527 51.8795 67.7 46.5 67.7Z" fill="black"/>
         <defs>
-            <filter id="filter0_b_464_2280" x="-15" y="-15" width="123" height="149" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <filter id="filter0_b_710_286" x="-15" y="-15" width="123" height="149" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
                 <feFlood flood-opacity="0" result="BackgroundImageFix"/>
                 <feGaussianBlur in="BackgroundImageFix" stdDeviation="7.5"/>
-                <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_464_2280"/>
-                <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_464_2280" result="shape"/>
+                <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_710_286"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_710_286" result="shape"/>
             </filter>
         </defs>
     </svg>
