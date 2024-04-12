4748898551628df2c6b3d539d7f991b17e3c5bab
P1su <pissssssu@naver.com>
2024-04-12 10:42:53 +0900

    chore: 필요없는 import문 삭제

--- src/components/common/buttons/Modal/ModalButton.style.js
+++ src/components/common/buttons/Modal/ModalButton.style.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import SvgIcModalHome from '../../../../assets/svg/IcModalHome'
-import SvgIcModalKeep from '../../../../assets/svg/IcModalKeep'
 
 export const BtnWrapper = styled.button`
     background-color: transparent;
