1eb3b16628579efd9083300aafd5b098ac290d8f
funcsom <bishop@kau.kr>
2024-04-12 17:26:42 +0900

    feat: 메인 페이지에 필요한 아이콘들

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,1 +1,6 @@
 export { default as IcError } from './IcError';
+
+export { default as IcThisMonthEmotion} from './IcThisMonthEmotion';
+export { default as IcMyPage} from './IcMyPage';
+export { default as IcSearchByEmotion} from './IcSearchByEmotion';
+export { default as IcThisMonthEmotion} from './IcWriteDiaryButton';
