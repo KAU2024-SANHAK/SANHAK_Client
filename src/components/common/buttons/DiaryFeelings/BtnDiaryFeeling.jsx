d30893c71beee9da071f8c2bc51717067b8647ae
P1su <pissssssu@naver.com>
2024-04-12 21:47:43 +0900

    feat: 감정 버튼에 button type 추가

--- src/components/common/buttons/DiaryFeelings/BtnDiaryFeeling.jsx
+++ src/components/common/buttons/DiaryFeelings/BtnDiaryFeeling.jsx
@@ -3,7 +3,7 @@ import * as S from './BtnDiaryFeeling.style'
 export default function BtnDiaryFeeling(props){
     
     return(
-        <S.DiaryFeelingButtonWrapper onClick={()=>{props.setFeeling(props.text)}}>
+        <S.DiaryFeelingButtonWrapper type='button' onClick={()=>{props.setFeeling(props.text)}}>
             <S.DiaryFeelingButton color={props.feeling === props.text ? props.color : 'white'} text={props.text}/>
         </S.DiaryFeelingButtonWrapper>
 
