af47094187b865b47d910cb1e6ec43e341019625
P1su <pissssssu@naver.com>
2024-04-13 00:05:35 +0900

    style: camelCase 수정

--- src/assets/svg/IcPrevButton.jsx
+++ src/assets/svg/IcPrevButton.jsx
@@ -1,7 +1,7 @@
 const SvgIcPrevButton=()=>(
     <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect width="60" height="60" rx="30" fill="white"/>
-        <path d="M33 21L24.7071 29.2929C24.3166 29.6834 24.3166 30.3166 24.7071 30.7071L33 39" stroke="black" stroke-width="4" strokeLinecap="round"/>
+        <path d="M33 21L24.7071 29.2929C24.3166 29.6834 24.3166 30.3166 24.7071 30.7071L33 39" stroke="black" strokeWidth="4" strokeLinecap="round"/>
     </svg>
 );
 export default SvgIcPrevButton;
