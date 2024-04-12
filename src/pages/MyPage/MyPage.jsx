6c0a16727779bb3e681fc005a8b43b6bff0ab66e
funcsom <bishop@kau.kr>
2024-04-12 19:10:38 +0900

    feat: MyPage 우선 커밋! 이후 mainpage로 올릴 것

---
+++ src/pages/MyPage/MyPage.jsx
@@ -0,0 +1,10 @@
+import * as S from './MyPage.style'
+import MyPageButton from '../../components/common/buttons/MyPageButton/MyPageButton'
+
+export default function MyPage(){
+    return(
+        <S.Wrapper>
+            <MyPageButton/>
+        </S.Wrapper>
+    )
+}
