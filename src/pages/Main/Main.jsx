dbf366a8bd188bb4bf12d9d259be31c79e538e97
funcsom <bishop@kau.kr>
2024-04-12 17:27:17 +0900

    feat: main page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,9 +1,11 @@
 import * as S from './Main.style'
+import MyPageButton from '../../components/common/buttons/MyPageButton/MyPageButton'
 
 export default function Main(){
     return(
         <S.Wrapper>
             <h1>메인 화면입니다.</h1>
+            
         </S.Wrapper>
     )
 }
--- src/pages/Main/Main.style.js
+++ src/pages/Main/Main.style.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  align-items: center;
   width: 100%;
   height: 100vh;
 `;
