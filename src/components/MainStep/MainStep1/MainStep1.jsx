ea26dcba3dd1e9e5febe2107d3ab5d28d2f47f17
funcsom <bishop@kau.kr>
2024-04-14 01:23:09 +0900

    feat: 이번달 감정 컴포넌트 (svg파일이 이상해서 우선 마이페이지 파일로 대체)

--- src/components/MainStep/MainStep1/MainStep1.jsx
+++ src/components/MainStep/MainStep1/MainStep1.jsx
@@ -3,6 +3,7 @@ import MyPageButton from '../../common/buttons/MyPageButton/MyPageButton'
 import SearchByEmotionButton from '../../common/buttons/SearchByEmotionButton/SearchByEmotionButton'
 import WriteDiaryButton from '../../common/buttons/WriteDiaryButton/WriteDiaryButton'
 import HoneyBear from '../../common/HoneyBear/HoneyBear'
+import ThisMonthEmotion from '../../common/buttons/ThisMonthEmotion/ThisMonthEmotion'
 
 export default function MainStep1(){
     return(
@@ -10,6 +11,8 @@ export default function MainStep1(){
             <MyPageButton/>  
             <SearchByEmotionButton/>   
             <WriteDiaryButton/>
+            <ThisMonthEmotion/>
+
             <S.MainStep1HoneyBearWrapper>
                 <HoneyBear height='50rem'/>
             </S.MainStep1HoneyBearWrapper>
--- src/components/common/buttons/ThisMonthEmotion/ThisMonthEmotion.style.js
+++ src/components/common/buttons/ThisMonthEmotion/ThisMonthEmotion.style.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
-import svgThisMonthEmotion from "../../../../assets/svg/IcThisMonthEmotion"
+import svgThisMonthEmotion from "../../../../assets/svg/IcMyPageButton";
 
 export const ThisMonthEmotionIcon = styled(svgThisMonthEmotion)`
     display: flex;
 `
 
-export const Wrapper = styled.button`
-width: 210px;
-height: 182px;
-flex-shrink: 0;
-border-radius: 30px;
-background: var(--Warm-Pink, linear-gradient(180deg, rgba(255, 255, 255, 0.00) 50%, rgba(255, 255, 255, 0.39) 111.04%), #FFA2A2);
+export const Wrapper = styled.div`
+// width: 210px;
+// height: 182px;
+// flex-shrink: 0;
+// border-radius: 30px;
+// background: var(--Warm-Pink, linear-gradient(180deg, rgba(255, 255, 255, 0.00) 50%, rgba(255, 255, 255, 0.39) 111.04%), #FFA2A2);
 `
 
 export const ThisMonthEmotionText = styled.p`
