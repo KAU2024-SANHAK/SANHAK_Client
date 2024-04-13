7a3ac077cac65e55c080666b9338c87f13b53713
P1su <pissssssu@naver.com>
2024-04-13 19:58:03 +0900

    style: Feeling 버튼 font 수정

--- src/components/common/buttons/DiaryFeelings/BtnDiaryFeeling.style.js
+++ src/components/common/buttons/DiaryFeelings/BtnDiaryFeeling.style.js
@@ -2,8 +2,11 @@ import styled from 'styled-components'
 import { IcDiaryFeeling } from "../../../../assets/svg"
 
 export const DiaryFeelingButtonWrapper = styled.button`
+    ${({ theme }) => theme.fonts.heading_01};
+    width: 9.6rem;
     background-color: transparent;
     border: none;
+    text-align: center;
 `
 export const DiaryFeelingButton = styled(IcDiaryFeeling)`
     display: flex;
