4caaa4d475e013b8ccc4a009e0d90a8ed0f473c2
P1su <pissssssu@naver.com>
2024-04-13 21:11:36 +0900

    style: backgroung color 설정

--- src/pages/MyPage/MyPage.style.js
+++ src/pages/MyPage/MyPage.style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const MyPageWrapper=styled.div`
+    background: ${({ theme }) => theme.colors.gradient.gradient_pink};
     width: 100%;
     height: 100vh;
 `
