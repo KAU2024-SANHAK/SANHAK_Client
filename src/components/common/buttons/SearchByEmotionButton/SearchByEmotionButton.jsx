c839fdbe3d018accd18a665d5ed27759ba1729d0
funcsom <bishop@kau.kr>
2024-04-13 11:53:23 +0900

    feat: 감정별로 일기조회 버튼 커밋

--- src/components/common/buttons/SearchByEmotionButton/SearchByEmotionButton.jsx
+++ src/components/common/buttons/SearchByEmotionButton/SearchByEmotionButton.jsx
@@ -3,9 +3,17 @@ import * as S from "./SearchByEmotionButton.style"
 
 export default function SearchByEmotionButton(props) {
     const navigate = useNavigate();
-    reutrn (
-        <S.Wrapper onClick={navigate("/searchbyemotion")}> // SearchByEmotion 함수를 불러오도록 함
-            감정별로 일기조회
+    
+    const handleClick = () => {
+        navigate('/mypage')
+    }
+
+    return (
+        <S.Wrapper onClick={handleClick}>
+            <S.SearchByEmotionButtonIcon/>
+            <S.SearchByEmotionButtonText>
+                감정별 일기조회
+            </S.SearchByEmotionButtonText>
         </S.Wrapper>
     )
 }
--- src/components/common/buttons/SearchByEmotionButton/SearchByEmotionButton.style.js
+++ src/components/common/buttons/SearchByEmotionButton/SearchByEmotionButton.style.js
@@ -1,7 +1,19 @@
 import styled from "styled-components";
+import svgSearchByEmotionButton from "../../../../assets/svg/IcSearchByEmotionButton";
+
+export const SearchByEmotionButtonIcon = styled(svgSearchByEmotionButton)`
+    display: flex;
+`
 
 export const Wrapper = styled.button`
-    width: 20rem;
-    height: 15rem;
-    border-radius: 12px;
+width: 9.3rem;
+height: 11.9rem;
+flex-shrink: 0;
+border-radius: 12px;
+background: rgba(255, 255, 255, 0.27);
+backdrop-filter: blur(7.5px);
+`
+
+export const SearchByEmotionButtonText = styled.p`
+    ${({ theme }) => theme.fonts.caption_01};
 `
