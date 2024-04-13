5e8fc34b0577fb6b5ebe095fd0a827eddccffb92
funcsom <bishop@kau.kr>
2024-04-13 12:15:14 +0900

    feat: main에 일기쓰기 버튼 추가 (아직 라우터 제대로 연결되진 않음)

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,12 +1,14 @@
 import * as S from './Main.style'
 import MyPageButton from '../../components/common/buttons/MyPageButton/MyPageButton'
 import SearchByEmotionButton from '../../components/common/buttons/SearchByEmotionButton/SearchByEmotionButton'
+import WriteDiaryButton from '../../components/common/buttons/WriteDiaryButton/WriteDiaryButton'
 
 export default function Main(){
     return(
         <S.Wrapper>
             <MyPageButton/>  
-            <SearchByEmotionButton/>    
+            <SearchByEmotionButton/>   
+            <WriteDiaryButton/> 
         </S.Wrapper>
     )
 }
