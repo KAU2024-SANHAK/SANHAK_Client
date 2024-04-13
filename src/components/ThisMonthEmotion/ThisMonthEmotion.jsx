b4cbcfd9fa9c7f5f3ee08de6d19f1b74c9e16c30
funcsom <bishop@kau.kr>
2024-04-13 13:38:08 +0900

    feat: 이번달 감정 키워드 컴포넌트 생성

--- src/components/ThisMonthEmotion/ThisMonthEmotion.jsx
+++ src/components/ThisMonthEmotion/ThisMonthEmotion.jsx
@@ -1,9 +1,13 @@
 import * as S from "./ThisMonthEmotion.style"
 
-export default function ThisMonthEmotion(props) {
+export default function ThisMonthEmotion() {
+
     return (
         <S.Wrapper>
-            
+            <S.ThisMonthEmotionIcon/>
+            <S.ThisMonthEmotionText>
+                api를 받아와야 해요.
+            </S.ThisMonthEmotionText>
         </S.Wrapper>
     )
 }
--- src/components/ThisMonthEmotion/ThisMonthEmotion.style.js
+++ src/components/ThisMonthEmotion/ThisMonthEmotion.style.js
@@ -1,5 +1,18 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import svgThisMonthEmotion from "../../assets/svg/IcThisMonthEmotion"
 
-export const Wrapper = styled.div`
-    ${({ theme: { mixin } }) => mixin.flexCenter({})}
+export const ThisMonthEmotionIcon = styled(svgThisMonthEmotion)`
+    display: flex;
+`
+
+export const Wrapper = styled.button`
+width: 210px;
+height: 182px;
+flex-shrink: 0;
+border-radius: 30px;
+background: var(--Warm-Pink, linear-gradient(180deg, rgba(255, 255, 255, 0.00) 50%, rgba(255, 255, 255, 0.39) 111.04%), #FFA2A2);
+`
+
+export const ThisMonthEmotionText = styled.p`
+    ${({ theme }) => theme.fonts.body_01};
 `
