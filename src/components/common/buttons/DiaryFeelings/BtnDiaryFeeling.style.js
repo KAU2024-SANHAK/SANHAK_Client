ecd6d33472d3b859dd47d74ec50625172bd3621a
P1su <pissssssu@naver.com>
2024-04-13 19:58:38 +0900

    style: 텍스트 정렬 관련 주석 추가

--- src/components/common/buttons/DiaryFeelings/BtnDiaryFeeling.style.js
+++ src/components/common/buttons/DiaryFeelings/BtnDiaryFeeling.style.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { IcDiaryFeeling } from "../../../../assets/svg"
 
+//버튼 텍스트 정렬이 안됩니다....
 export const DiaryFeelingButtonWrapper = styled.button`
     ${({ theme }) => theme.fonts.heading_01};
     width: 9.6rem;
