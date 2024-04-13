9bf2efc0af6c58f8dc191a18429a67e63814925c
funcsom <bishop@kau.kr>
2024-04-14 00:16:28 +0900

    feat: 전체 페이지 구조를 한 번 바꾸었습니다! main 페이지 내에서 직접 버튼을 호출하는 것이 아닌, step1에 버튼을 모두 호출하고, 그것을 main에서 가져다 쓸 수 있도록 바꾸었습니다. 수정에 용이한 구조라고 생각되어 필수오빠꺼 따라함

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,14 +1,10 @@
 import * as S from './Main.style'
-import MyPageButton from '../../components/common/buttons/MyPageButton/MyPageButton'
-import SearchByEmotionButton from '../../components/common/buttons/SearchByEmotionButton/SearchByEmotionButton'
-import WriteDiaryButton from '../../components/common/buttons/WriteDiaryButton/WriteDiaryButton'
+import MainStep1 from '../../components/MainStep/MainStep1/MainStep1'
 
 export default function Main(){
     return(
         <S.Wrapper>
-            <MyPageButton/>  
-            <SearchByEmotionButton/>   
-            <WriteDiaryButton/> 
+            <MainStep1/>
         </S.Wrapper>
     )
 }
