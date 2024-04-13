734c73028725b39767743529347de6ac1c3ec515
P1su <pissssssu@naver.com>
2024-04-13 22:49:52 +0900

    feat: 일기 작성 스타일 설정 모달창을 여는 버튼 생성

---
+++ src/components/common/buttons/SetDiaryModal/BtnSetDiaryModal.jsx
@@ -0,0 +1,10 @@
+import * as S from './BtnSetDiaryModal.style'
+
+//충격적인 변수명,, 그렇지만 좋은 게 떠오르지가 않네요
+export default function BtnSetDiaryModal(){
+    return(
+        <S.ButtonWrapper>
+            빠른 일기    
+        </S.ButtonWrapper>
+    )
+}
---
+++ src/components/common/buttons/SetDiaryModal/BtnSetDiaryModal.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ButtonWrapper=styled.button`
+    ${({ theme }) => theme.fonts.body_01};
+    fill: rgba(255, 255, 255, 0.50);
+    width: 32rem;
+    height: 5.2rem;
+
+    font-weight: 400;
+`
