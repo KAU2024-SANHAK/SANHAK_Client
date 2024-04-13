d908e9a46541e1cf289a89ebcc97a59945da5f0e
P1su <pissssssu@naver.com>
2024-04-13 19:50:17 +0900

    style: FeelingHive 레이아웃 설정

--- src/components/FastDiarySteps/FeelingHive/FeelingHive.jsx
+++ src/components/FastDiarySteps/FeelingHive/FeelingHive.jsx
@@ -8,13 +8,24 @@ export default function FeelingHive(){
 
     return(
         <S.FeelingHiveWrapper>
-                <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#FFF1A8' text = '기쁨'/>
-                <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#B4F2FF' text = '슬픔'/>
-                <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#FFB6B6' text = '분노'/>
-                <IcHoneyPot/>
-                <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#C3C9FF' text = '걱정'/>
-                <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#B9FFB3' text = '평온'/>
+            <S.FirstLayer>
                 <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#FFCA99' text = '놀람'/>
+                <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#B9FFB3' text = '평온'/>
+            </S.FirstLayer>
+
+            <S.SecondLayer>
+                <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#FFF1A8' text = '기쁨'/>
+                <S.HoneyPotWrapper>
+                    <IcHoneyPot/>
+                </S.HoneyPotWrapper>
+                <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#C3C9FF' text = '걱정'/>
+            </S.SecondLayer>
+
+            <S.ThirdLayer>
+                <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#B4F2FF' text = '슬픔'/>        
+                <BtnDiaryFeeling feeling = {feeling} setFeeling = {setFeeling} color = '#FFB6B6' text = '분노'/>
+            </S.ThirdLayer>
+
         </S.FeelingHiveWrapper>
     )
 }
--- src/components/FastDiarySteps/FeelingHive/FeelingHive.style.js
+++ src/components/FastDiarySteps/FeelingHive/FeelingHive.style.js
@@ -1,7 +1,26 @@
 import styled from 'styled-components'
 
 export const FeelingHiveWrapper = styled.div`
-    display: center;
-    align-items: center;
-    justify-content: center;
+${({ theme: { mixin } }) => mixin.flexBox({justify: 'space-between', align:'center'})};
+width: 26rem;
+margin-bottom: 10rem;
+margin-right: 3rem;
+`
+export const FirstLayer= styled.div`
+    width: 7rem;
+    height: 10rem;
+    margin: auto;
+`
+export const SecondLayer= styled.div`
+    width: 7rem;
+    height: 18.3rem;
+    margin: auto;
+`
+export const ThirdLayer= styled.div`
+    width: 7rem;
+    height: 10rem;
+    margin: auto;
+`
+export const HoneyPotWrapper = styled.div`
+    padding-left: 0.8rem;
 `
