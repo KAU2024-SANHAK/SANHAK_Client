a930b5c91b797fd88d973ee900d0135d9b922ee5
funcsom <bishop@kau.kr>
2024-04-13 11:55:48 +0900

    feat: 감정별 일기조회 페이지 임의로 만듦!

---
+++ src/pages/SearchByEmotion/SearchByEmotion.jsx
@@ -0,0 +1,9 @@
+import * as S from './SearchByEmotion.style'
+
+export default function SearchByEmotion(){
+    return(
+        <S.Wrapper>
+            <p>감정별 일기조회 페이지입니다.</p>
+        </S.Wrapper>
+    )
+}
