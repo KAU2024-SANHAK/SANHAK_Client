8b04f4d9ec9a8af764ae920644f107948bb4fa9d
P1su <pissssssu@naver.com>
2024-04-14 22:35:09 +0900

    style: 버튼 컴포넌트 position: relative 설정

--- src/components/common/buttons/SetDiaryModal/BtnSetDiaryModal.style.js
+++ src/components/common/buttons/SetDiaryModal/BtnSetDiaryModal.style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const ButtonWrapper=styled.button`
     ${({ theme }) => theme.fonts.body_01};
+    position: relative;
     fill: rgba(255, 255, 255, 0.50);
     width: 32rem;
     height: 5.2rem;
