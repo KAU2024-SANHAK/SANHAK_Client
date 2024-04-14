b70ad7ee9a14b33cfc715028edd6858c406eb356
P1su <pissssssu@naver.com>
2024-04-14 23:13:44 +0900

    feat: HoneyBear 배치

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,9 +1,13 @@
-import PopUp from '../../components/PopUp/PopUp'
 import * as S from './MyPage.style'
+import PopUp from '../../components/PopUp/PopUp'
+
 
 export default function MyPage(){
     return(
         <S.MyPageWrapper>
+            <S.HoneyBearWrapper>
+                <S.HoneyBear height='24.65rem'/>
+            </S.HoneyBearWrapper>
             <PopUp>
                 마이페이지
             </PopUp>
