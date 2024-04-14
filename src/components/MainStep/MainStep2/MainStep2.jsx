1d4979bbb83f18002de810cae600cff56b94765b
funcsom <bishop@kau.kr>
2024-04-14 13:53:29 +0900

    feat: 메인페이지 2

--- src/components/MainStep/MainStep2/MainStep2.jsx
+++ src/components/MainStep/MainStep2/MainStep2.jsx
@@ -1,18 +1,9 @@
 import * as S from './MainStep2.style'
-import MyPageButton from '../../common/buttons/MyPageButton/MyPageButton'
-import SearchByEmotionButton from '../../common/buttons/SearchByEmotionButton/SearchByEmotionButton'
-import WriteDiaryButton from '../../common/buttons/WriteDiaryButton/WriteDiaryButton'
 import HoneyBearMainStep2 from '../../common/HoneyBearMainStep2/HoneyBearMainStep2'
-import ThisMonthEmotion from '../../common/buttons/ThisMonthEmotion/ThisMonthEmotion'
 
 export default function MainStep2(){
     return(
         <S.Wrapper>
-            <MyPageButton/>  
-            <SearchByEmotionButton/>   
-            <WriteDiaryButton/>
-            <ThisMonthEmotion/>
-
             <S.MainStep1HoneyBearWrapper>
                 <HoneyBearMainStep2 height='100rem'/>
             </S.MainStep1HoneyBearWrapper>
