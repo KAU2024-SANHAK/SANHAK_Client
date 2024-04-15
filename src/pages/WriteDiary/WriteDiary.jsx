826ef63c573d8305a4362fd1371cb625a4aa01e3
funcsom <bishop@kau.kr>
2024-04-15 17:07:00 +0900

    feat: 호은이랑 수정한 것

--- src/pages/WriteDiary/WriteDiary.jsx
+++ src/pages/WriteDiary/WriteDiary.jsx
@@ -1,9 +1,11 @@
 import * as S from './WriteDiary.style'
 
-export default function WriteDiary(){
+function WriteDiary(){
     return(
         <S.Wrapper>
             <p>일기 작성 화면입니다.</p>     
         </S.Wrapper>
     )
 }
+
+export default WriteDiary;
