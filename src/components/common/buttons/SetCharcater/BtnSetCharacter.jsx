a5eee6e74757e57950daa34397ded22b35b27ff5
P1su <pissssssu@naver.com>
2024-04-15 10:27:33 +0900

    style: 주석 수정

--- src/components/common/buttons/SetCharcater/BtnSetCharacter.jsx
+++ src/components/common/buttons/SetCharcater/BtnSetCharacter.jsx
@@ -2,7 +2,7 @@ import * as S from './BtnSetCharacter.style'
 
 export default function BtnSetCharacter({onClick}){
     return(
-        <S.ButtonWrapper onClick={()=>{onClick}}>
+        <S.ButtonWrapper onClick={()=>{onClick()}}>
             캐릭터 도감 보러가기
         </S.ButtonWrapper>
     )
--- src/components/common/buttons/SetDiaryModal/BtnSetDiaryModal.jsx
+++ src/components/common/buttons/SetDiaryModal/BtnSetDiaryModal.jsx
@@ -2,7 +2,7 @@ import * as S from './BtnSetDiaryModal.style'
 
 //충격적인 변수명,, 그렇지만 좋은 게 떠오르지가 않네요
 //자신의 현재 상태에 따라 텍스트값 렌더링되도록 수정
-//이거 온보딩때 recoil로 저장해두고, 여기서 다시 불러와야겠네요. 
+//이거 온보딩때 recoil로 저장해두고, 여기서 다시 불러와야겠네요
 export default function BtnSetDiaryModal({onClick}){
     return(
         <S.ButtonWrapper onClick={()=>{onClick()}}>
