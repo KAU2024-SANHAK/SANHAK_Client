b029de932a6c7cb0734218a89690b1d696625845
P1su <pissssssu@naver.com>
2024-04-15 23:12:35 +0900

    feat: 캐릭터 도감 페이지 style component 및 버튼 설정 후 배치

--- src/pages/CharacterWiki/CharacterWiki.jsx
+++ src/pages/CharacterWiki/CharacterWiki.jsx
@@ -1,9 +1,17 @@
 import * as S from './CharacterWiki.style'
+import BtnBack from '../../components/common/buttons/Back/BtnBack'
 
 export default function CharacterWiki(){
     return(
         <S.CharacterWikiPageWrapper>
-            
+            <BtnBack/>
+            <h1>gi</h1>
+            <S.HoneyBearWikiWrapper>
+                곰돌이
+            </S.HoneyBearWikiWrapper>
+            <S.HoneyBeeWikiWrapper>
+                꿀벌
+            </S.HoneyBeeWikiWrapper>
         </S.CharacterWikiPageWrapper>
     )
 }
--- src/pages/CharacterWiki/CharacterWiki.style.js
+++ src/pages/CharacterWiki/CharacterWiki.style.js
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 
 export const CharacterWikiPageWrapper=styled.div`
+    ${({ theme: { mixin } }) => mixin.flexCenter({})};
+    background-color: ${({ theme }) => theme.colors.pink.white_pink};
     width: 100%;
     height: 100vh;
 `
+export const HoneyBearWikiWrapper=styled.div`
+
+` 
+export const HoneyBeeWikiWrapper=styled.div`
+
+` 
