52b1f929e27d77c51ab0f3a8551a22c55b9651fd
P1su <pissssssu@naver.com>
2024-04-17 02:04:35 +0900

    feat: 마이페이지에서 메인으로 navigate 설정

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,13 +1,19 @@
 import * as S from './MyPage.style'
+import { useNavigate } from 'react-router-dom'
 import PopUp from '../../components/PopUp/PopUp'
 import BtnBack from '../../components/common/buttons/Back/BtnBack'
 import MyPagePopUp from './MyPagePopUp/MyPagePopUp'
 
 export default function MyPage(){
+    const navigate=useNavigate();
+    const GoMain=()=>{
+        navigate('/main');
+    }
+
     return(
         <S.MyPageWrapper>
             <S.BackButtonWrapper>
-                <BtnBack onClick={()=>{GoWiki()}}/>
+                <BtnBack onClick={()=>{GoMain()}}/>
             </S.BackButtonWrapper>
             
             <S.HoneyBearWrapper>
