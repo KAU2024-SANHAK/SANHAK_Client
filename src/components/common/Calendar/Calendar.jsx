1d8e2241c62d0a595bb30dde20fc7ffec1ca7689
funcsom <bishop@kau.kr>
2024-04-16 17:44:22 +0900

    feat: caledar 컴포넌트 구현

---
+++ src/components/common/Calendar/Calendar.jsx
@@ -0,0 +1,9 @@
+import * as S from "./Calendar.style"
+
+export default function Calendar() {
+    return (
+        <S.Wrapper>
+            <S.Calendar/>
+        </S.Wrapper>
+    )
+}
---
+++ src/components/common/Calendar/Calendar.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Calendar = styled.div`
+    display: flex;
+    width: 320px;
+    height: 427px;
+    flex-shrink: 0;
+`
