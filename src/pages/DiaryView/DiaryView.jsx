3d40be7f0f61e006b3623ee0ece111596aaff776
funcsom <bishop@kau.kr>
2024-04-17 22:07:30 +0900

    feat: diaryview 내 popup 구현

--- src/pages/DiaryView/DiaryView.jsx
+++ src/pages/DiaryView/DiaryView.jsx
@@ -1,10 +1,15 @@
 import * as S from './DiaryView.style'
 import CreatedDiary from '../../components/CreatedDiary/CreatedDiary'
+import PopUp from '../../components/PopUp/PopUp'
+// import MyPagePopUp from '../../components/PopUp/ReplyPopUp/ReplyPopUp'
 
 export default function DiaryView(){
     return(
         <S.DiaryViewPageWrapper>
             <CreatedDiary/>
+            <PopUp name={'꿀비의 답장'}>
+                {/* <MyPagePopUp/> */}
+            </PopUp>
         </S.DiaryViewPageWrapper>
     )
 }
