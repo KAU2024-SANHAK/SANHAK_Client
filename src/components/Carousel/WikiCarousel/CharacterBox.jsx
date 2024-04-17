efc7b6a6b664daff7d6cbda516687e82f665349b
P1su <pissssssu@naver.com>
2024-04-17 18:31:20 +0900

    style: 들여쓰기 수정

--- src/components/Carousel/WikiCarousel/CharacterBox.jsx
+++ src/components/Carousel/WikiCarousel/CharacterBox.jsx
@@ -3,13 +3,13 @@ import * as S from './WikiCarousel.style'
 export default function CharacterBox({children, name}){
     return(
         <S.CharacterBoxWrapper>
-        <S.CharacterBox>
-            {children}
-        </S.CharacterBox>
-        
-        <S.CostumeName>
-            {name}
-        </S.CostumeName>
+            <S.CharacterBox>
+                {children}
+            </S.CharacterBox>
+            
+            <S.CostumeName>
+                {name}
+            </S.CostumeName>
         </S.CharacterBoxWrapper>
     )
 }
