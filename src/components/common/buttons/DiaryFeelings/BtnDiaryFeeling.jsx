407c33d51bc356a5f13a725591bc9ac0f14b75e2
P1su <pissssssu@naver.com>
2024-04-18 23:39:52 +0900

    feat: DiaryFeelingButton의 텍스트값을 svg가 아니라 컴포넌트에서 나타내도록 구현

--- src/components/common/buttons/DiaryFeelings/BtnDiaryFeeling.jsx
+++ src/components/common/buttons/DiaryFeelings/BtnDiaryFeeling.jsx
@@ -3,7 +3,8 @@ import * as S from './BtnDiaryFeeling.style'
 export default function BtnDiaryFeeling({feeling, setFeeling, color, text}){
     return(
         <S.DiaryFeelingButtonWrapper type='button' onClick={()=>{setFeeling(text)}}>
-            <S.DiaryFeelingButton color={feeling === text ? color : 'white'} text={text}/>
+            <S.FeelingText>{text}</S.FeelingText>
+            <S.DiaryFeelingButton color={feeling === text ? color : 'white'}/>
         </S.DiaryFeelingButtonWrapper>
 
     )
--- src/components/common/buttons/DiaryFeelings/BtnDiaryFeeling.style.js
+++ src/components/common/buttons/DiaryFeelings/BtnDiaryFeeling.style.js
@@ -2,13 +2,19 @@ import styled from 'styled-components'
 import { IcDiaryFeeling } from "../../../../assets/svg"
 
 export const DiaryFeelingButtonWrapper = styled.button`
+    ${({ theme: { mixin } }) => mixin.flexCenter({})};
     ${({ theme }) => theme.fonts.heading_01};
     width: 9.6rem;
     background-color: transparent;
     border: none;
+    padding-left: 2rem;
     text-align: center;
+    font-weight: 700;
+    letter-spacing: -0.0408rem;
 `
 export const DiaryFeelingButton = styled(IcDiaryFeeling)`
-    position: relative;
     display: flex;
+`
+export const FeelingText=styled.p`
+    position: absolute;
 `
