d8e7770d4a0a8db5b8583c15fd139c2311e92cdd
P1su <pissssssu@naver.com>
2024-04-19 22:41:28 +0900

    feat: client.js 에 instance 3개 설정

--- src/apis/client.js
+++ src/apis/client.js
@@ -1,8 +1,25 @@
 import axios from "axios";
 
 export const authInstance = axios.create({
-    baseURL: "",
-    timeout: 1000,
+    baseURL: import.meta.env.VITE_APP_BASE_URL,
     withCredentials: true,
-    headers:{"X-Custom-Header": ""}
+    headers:{'X-Environment': 
+    ''
+    }
+})
+
+export const instance = axios.create({
+    baseURL: import.meta.env.VITE_APP_BASE_URL,
+    withCredentials: true,
+    headers:{
+        Authorization: Bearer 
+    }
+})
+
+export const aiInstance = axios.create({
+    baseURL: import.meta.env.VITE_AI_BASE_URL,
+    withCredentials: true,
+    headers:{
+        Authorization: Bearer 
+    }
 })
