d9314195c444e1dfd81eb89b2c794c1fe96190da
P1su <pissssssu@naver.com>
2024-04-19 23:27:29 +0900

    feat: instance를 이용하여 useLogin 재구현

--- src/hooks/queries/member/members/useLogin.jsx
+++ src/hooks/queries/member/members/useLogin.jsx
@@ -2,35 +2,35 @@ import { useNavigate } from "react-router-dom";
 import { authInstance } from "../../../../apis/client";
 import { useEffect } from "react";
 
-const params = new URL(document.URL).searchParams;
-const code = params.get('code');
-
-//백엔드 서버에 get 으로 넘겨주기
 const useLogin= async () => {
     const navigate = useNavigate();
+    const params = new URL(document.URL).searchParams;
+    const code = params.get('code');   
     
+    const fetchAuth = ()=>{
+        authInstance.get(`/api/ouath/kakao/login?code=${code}`)
+    }
+
     useEffect(()=>{ 
 
         if(code){
-            try{
-                const response = authInstance.get(
-                //서버 주소에 쿼리형식으로 인가코드 넣어
-                `${code}`
-                )
-                
-                //const userData = response.data.;
-            
+            fetchAuth().then((response) =>{
+                const data = response.data
 
-            }catch(error){
-                console.log(error)
-            }
+                const JWT = data.data.memberToken.accessToken;
+                const RefreshToken = data.data.memberToken.refreshToken;
+
+                localStorage.setItem('LOGIN_TOKEN', JWT);
+                localStorage.setItem('LOGIN_REFRESHTOKEN', RefreshToken);
+                //
+                //localStorage.setItem('최초 로그인 여부 판단' 어쩌구 저쩌구)
+
+            })
             
-            //나중에 최초 로그인 값, 실제 아이디, 토큰 값 등으로 수정하겠습니다.
-            localStorage.setItem('EXIT_LOGIN_TOKEN', true);
         }
 
         //if 최초 로그인이면 온보딩으로 이동하기
-        navigate('/onboarding')
+        navigate('/')
         //else 메인 화면으로
     }, [])
 }
