062ef1cc0bcbbdb1d3e18cd0f40da5abf204c757
P1su <pissssssu@naver.com>
2024-04-19 19:17:05 +0900

    feat: handleNext 함수 추가 후 클릭 이벤트와 함께 페이지 이동

--- src/pages/SlowDiary/SlowDiary.jsx
+++ src/pages/SlowDiary/SlowDiary.jsx
@@ -1,6 +1,7 @@
 import * as S from './SlowDiary.style'
 import { useState } from 'react';
 import { useModal } from '../../hooks/common/useModal';
+import { useNavigate } from 'react-router-dom';
 import BtnHome from '../../components/common/buttons/Home/BtnHome';
 import BtnNext from '../../components/common/buttons/Next/BtnNext';
 import HomeModal from '../../components/Modal/HomeModal';
@@ -8,6 +9,7 @@ import HomeModal from '../../components/Modal/HomeModal';
 export default function SlowDiary(){
     const date = new Date();
     const today = date.getDate();
+    const navigate = useNavigate();
     const [isOpen, opneModal, closeModal] = useModal();
 
     const [data, setData] = useState({
@@ -18,7 +20,6 @@ export default function SlowDiary(){
 
     const handleSubmit=(event)=>{
         //api 연결
-        event.preventDefault();
         alert('저장!');
         console.log(data);
     }
@@ -37,6 +38,10 @@ export default function SlowDiary(){
 
     }
 
+    const handleNext=()=>{
+        navigate('/diaryview')
+    }
+
     return(
         <S.SlowDiaryPageWrapper  $isEven = {today%2}>
             {isOpen && <HomeModal closeModal = {closeModal}/>}
@@ -49,19 +54,21 @@ export default function SlowDiary(){
             </S.SlowDiaryHeader>
 
             <S.FormWrapper onSubmit={handleSubmit}>
-                <S.ImageInput type='file' id ='imgInput' accept='image/*' name='image'  onChange={handleImage}/>
+                <S.ImageInput type='file' id ='imgInput' accept='image/*' name='imageUrl' onChange={handleImage}/>
                 <S.Label htmlFor='imgInput'>
-                    {data.image ? (
-                        <S.PreviewImg src={data.imageUrl} alt='preview'/>
+                    {data.imageUrl ? (
+                        <S.PreviewImg src={data.imageUrl}/>
                     ): 
                         <S.AddImg/>
                     }
                 </S.Label>
-                <S.TitleInput type='text' placeholder='제목을 입력해주세요.' name='title' value={data.diaryTitle} onChange={handleChange}/>
-                <S.ContentInput type='text' placeholder='오늘 무슨 일이 있었나요? 자유롭게 적어주세요.' name='content' value={data.diaryContent} onChange={handleChange}/>
+                <S.TitleInput type='text' placeholder='제목을 입력해주세요.' name='diaryTitle' value={data.diaryTitle} onChange={handleChange}/>
+                <S.ContentInput type='text' placeholder='오늘 무슨 일이 있었나요? 자유롭게 적어주세요.' name='diaryContent' value={data.diaryContent} onChange={handleChange}/>
 
                 <S.BtnField>
-                    <BtnNext/>
+                    <BtnNext onNext={()=>{handleNext()
+                    handleSubmit()
+                    }}/>
                 </S.BtnField>
             </S.FormWrapper>
 
