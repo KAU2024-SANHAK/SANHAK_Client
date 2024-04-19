a0038320dfa51dea35d0c1b5d3bb156dded6c045
P1su <pissssssu@naver.com>
2024-04-19 17:24:21 +0900

    style: SubTitle과 Caption 컴포넌트 내부 들여스기 수정

--- src/components/OnBoardingSteps/title/SubTitle.jsx
+++ src/components/OnBoardingSteps/title/SubTitle.jsx
@@ -2,6 +2,8 @@ import * as S from './Title.style'
 
 export default function SubTitle({children}){
     return(
-        <S.SubTitleWrapper>{children}</S.SubTitleWrapper>
+        <S.SubTitleWrapper>
+            {children}
+        </S.SubTitleWrapper>
     )
 }
