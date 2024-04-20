869cbbd35af7ce1b5ef3a939e9c653965675bf06
P1su <pissssssu@naver.com>
2024-04-20 20:21:29 +0900

    feat: recoilPersist 적용

--- src/recoil/atoms.js
+++ src/recoil/atoms.js
@@ -1,6 +1,13 @@
- import { atom } from "recoil";
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } =recoilPersist({
+   key: 'localstorage',
+   storage: localStorage,
+})
 
 export const UserDiaryType = atom({
    key: 'userDiaryType',
    default: '',
+   effects_UNSTABLE: [persistAtom]
 });
