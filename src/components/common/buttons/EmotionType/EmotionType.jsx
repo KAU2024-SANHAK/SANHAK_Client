db18127527708aa690a7c36069be845583a3a0ca
funcsom <bishop@kau.kr>
2024-04-30 23:45:21 +0900

    fix: 감정별 일기조회 페이지에서 각각의 감정 Enum을 넘겨줄 수 있도록 코드 수정

--- src/components/common/buttons/EmotionType/EmotionType.jsx
+++ src/components/common/buttons/EmotionType/EmotionType.jsx
@@ -1,11 +1,16 @@
 import * as S from "./EmotionType.style";
 import { useNavigate } from "react-router-dom";
+// API 통신을 위한 import
+// import { usePostFeelingList } from "../../../../hooks/queries/mypage/usePostFeelingList";
 
-export default function EmotionType({name}) {
-    const navigate = useNavigate();
-
+export default function EmotionType({name, btnEnum}) {
+    const navigate=useNavigate();
     const handleClick = () => {
-        navigate(`/searchbyemotion/diarylist`);
+        // 백에서 API 가져와서 Feeling btnEnum 넘기고 해당 Feeling에 해당하는 일기를 불러오는 페이지로 들어가기
+        // const response = usePostFeelingList(btnEnum)
+        // navigate(`/searchbyemotion/diarylist/response?=${response}`);
+
+        navigate('/searchbyemotion/diarylist')
     }
     
     return (
