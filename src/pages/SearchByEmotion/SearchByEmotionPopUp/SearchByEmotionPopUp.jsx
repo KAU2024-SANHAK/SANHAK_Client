59a9147489b5ca9ec53dc514e7de3539a8600815
funcsom <bishop@kau.kr>
2024-04-30 23:43:01 +0900

    feat: SearchByEmotionPopUp에서 버튼 클릭 시, 해당 btnEnum을 넘겨주고 결국 Enum에 해당하는 다이어리 리스트를 불러오고자 하는 코드

--- src/pages/SearchByEmotion/SearchByEmotionPopUp/SearchByEmotionPopUp.jsx
+++ src/pages/SearchByEmotion/SearchByEmotionPopUp/SearchByEmotionPopUp.jsx
@@ -6,15 +6,15 @@ export default function SearchByEmotionPopUp(){
     return(
         <S.Wrapper>
             <S.SearchByEmotionPopUpWrapper1>
-                <EmotionType name='기쁨'/>
-                <EmotionType name='분노'/>
-                <EmotionType name='평온'/>
+                <EmotionType name='기쁨' btnEnum="HAPPY"/>
+                <EmotionType name='분노' btnEnum="ANGRY"/>
+                <EmotionType name='평온' btnEnum="RELAX"/>
             </S.SearchByEmotionPopUpWrapper1>
 
             <S.SearchByEmotionPopUpWrapper2>
-                <EmotionType name='슬픔'/>
-                <EmotionType name='공포'/>
-                <EmotionType name='놀람'/>
+                <EmotionType name='슬픔' btnEnum="SAD"/>
+                <EmotionType name='걱정' btnEnum="WORRIED"/>
+                <EmotionType name='놀람' btnEnum="SURPRISED"/>
             </S.SearchByEmotionPopUpWrapper2>
         </S.Wrapper>
         
