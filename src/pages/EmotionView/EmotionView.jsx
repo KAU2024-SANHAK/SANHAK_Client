0efd5123d6015b586cf04e8ef4c6a8f0f3fec8ab
funcsom <bishop@kau.kr>
2024-05-01 00:57:13 +0900

    FIX: 에러 및 워닝이 나는 사항들 수정하여 커밋

--- src/pages/EmotionView/EmotionView.jsx
+++ src/pages/EmotionView/EmotionView.jsx
@@ -1,11 +1,10 @@
 import * as S from './EmotionView.style';
 import { EmotionViewComponent } from '../../components/EmotionViewComponent/EmotionViewComponent';
 
-function EmotionView(){
+export default function EmotionView(){
     return(
         <S.EmotionViewWrapper>
             <EmotionViewComponent/>
         </S.EmotionViewWrapper>
     )
-}
-export default EmotionView;
+};
