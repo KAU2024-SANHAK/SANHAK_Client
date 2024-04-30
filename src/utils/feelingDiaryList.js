b1a2fd5d262dd32c923ce9d302f61d22345f1694
funcsom <bishop@kau.kr>
2024-04-30 23:48:05 +0900

    TEST: 테스트 케이스

---
+++ src/utils/feelingDiaryList.js
@@ -0,0 +1,32 @@
+export const feelingDiaryList = [
+    {
+        "diaryId": 1,
+        "createdDate": "2024-01-07T00:00:00",
+        "diaryTitle": "string1"
+    },
+    {
+        "diaryId": 2,
+        "createdDate": "2024-01-08T00:00:00",
+        "diaryTitle": "string2"
+    },
+    {
+        "diaryId": 3,
+        "createdDate": "2024-01-09T00:00:00",
+        "diaryTitle": "string3"
+    },
+    {
+        "diaryId": 4,
+        "createdDate": "2024-01-07T00:00:00",
+        "diaryTitle": "string1"
+    },
+    {
+        "diaryId": 5,
+        "createdDate": "2024-01-08T00:00:00",
+        "diaryTitle": "string2"
+    },
+    {
+        "diaryId": 6,
+        "createdDate": "2024-01-09T00:00:00",
+        "diaryTitle": "string3"
+    }
+];
