0f8d4ff49ebf1abbd749a40eb9cb4e25a6ea0d93
funcsom <bishop@kau.kr>
2024-05-02 10:36:51 +0900

    TEST: 테스트 코드

---
+++ src/utils/diaryContent.js
@@ -0,0 +1,16 @@
+export const diaryContent = [
+    {
+        "status": 201,
+        "message": "요청이 성공했습니다.",
+        "data":{
+            "diary_content":"string",
+            "advice":{
+                "spicy": "string",
+                "kind": "string",
+            },
+            "feeling": "HAPPY",
+            "imageUrl": "fsdfsfdsfdsf",
+            "diaryTitle":"오늘나의하루"
+        }
+    }
+];
