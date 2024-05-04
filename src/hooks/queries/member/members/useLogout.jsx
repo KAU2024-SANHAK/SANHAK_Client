79633458d1faf1afb519088f8afe7a1a28218215
Wang HoEun <hoeunwang@gmail.com>
2024-05-04 19:13:48 +0900

    fix: localstorage 삭제랑 api 코드 구분

--- src/hooks/queries/member/members/useLogout.jsx
+++ src/hooks/queries/member/members/useLogout.jsx
@@ -1,20 +1,17 @@
-import { serverInstance } from "../../../../apis/client";
-import { useMutation } from "react-query";
+import { serverInstance } from '../../../../apis/client';
+import { useMutation } from 'react-query';
 
 export const patchToken = async () => {
-    const response =  await serverInstance.patch('/api/log-out',{});
-    return response.data;
-}
+  const response = await serverInstance.patch('/api/log-out', {});
+  return response.data;
+};
 
 export const useLogout = () => {
-    const mutation = useMutation({
-        mutationFn: patchToken,
-        onSuccess: (data) => {
-            console.log(data.message);
-            localStorage.removeItem('LOGIN_TOKEN');
-            localStorage.removeItem('LOGIN_REFRESH_TOKEN');
-            navigate('/')
-        }
-    })
-    return { mutation }; 
+  const mutation = useMutation({
+    mutationFn: patchToken,
+    onSuccess: (data) => {
+      console.log(data.message);
+    },
+  });
+  return { mutation };
 };
--- src/pages/MyPage/MyPagePopUp/MyPagePopUp.jsx
+++ src/pages/MyPage/MyPagePopUp/MyPagePopUp.jsx
@@ -1,47 +1,67 @@
-import * as S from './MyPagePopUp.style'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useRecoilValue } from 'recoil'
-import { UserDiaryType } from '../../../recoil/atoms'
-import {useLogout} from '../../../hooks/queries/member/members/useLogout'
-import BtnLogout from '../../../components/common/buttons/Logout/BtnLogout'
-import BtnComplete from '../../../components/common/buttons/complete/BtnComplete'
-import BtnSetCharacter from '../../../components/common/buttons/SetCharcater/BtnSetCharacter'
-import BtnCurDiaryType from '../../../components/common/buttons/CurDiaryType/BtnCurDiaryType'
-import SetWritingStyle from '../../../components/SetDiaryStyle/SetDiaryStyle'
+import * as S from './MyPagePopUp.style';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { UserDiaryType } from '../../../recoil/atoms';
+import { useLogout } from '../../../hooks/queries/member/members/useLogout';
+import BtnLogout from '../../../components/common/buttons/Logout/BtnLogout';
+import BtnComplete from '../../../components/common/buttons/complete/BtnComplete';
+import BtnSetCharacter from '../../../components/common/buttons/SetCharcater/BtnSetCharacter';
+import BtnCurDiaryType from '../../../components/common/buttons/CurDiaryType/BtnCurDiaryType';
+import SetWritingStyle from '../../../components/SetDiaryStyle/SetDiaryStyle';
 
+export default function MyPagePopUp() {
+  const [isClick, setIsClick] = useState(false);
+  const curType = useRecoilValue(UserDiaryType);
+  const { mutation } = useLogout();
+  const navigate = useNavigate();
 
-export default function MyPagePopUp(){
-    const [isClick, setIsClick] = useState(false);
-    const curType = useRecoilValue(UserDiaryType);
-    const  {mutation}  = useLogout();
-    const navigate = useNavigate();
+  const handlegoWiki = () => {
+    navigate('/mypage/characterwiki');
+  };
 
-    const handlegoWiki = () => {
-        navigate('/mypage/characterwiki');
-    };
+  const handleLogout = () => {
+    mutation.mutate();
 
-    const handleLogout = () => {
-        mutation.mutate();
-    };
+    localStorage.removeItem('LOGIN_TOKEN');
+    localStorage.removeItem('LOGIN_REFRESH_TOKEN');
+    navigate('/');
+  };
 
-    return(
-        <S.MyPagePopUpWrapper>
-
-            <S.TextWrapper>일기 작성 스타일</S.TextWrapper>
-                {isClick === false ? 
-                <S.MyPagePopUpBodyWrapper>
-                    <BtnCurDiaryType onClick={()=>{setIsClick(true)}} curType={curType}/>
-                    <BtnSetCharacter onClick={()=>{handlegoWiki()}}/>
-                    <BtnLogout onClick={()=>{handleLogout()}}/>
-                </S.MyPagePopUpBodyWrapper>
-                :
-                <S.MyPagePopUpBodyWrapper>
-                    <SetWritingStyle transparency='100'/>
-                    <BtnComplete onClick={()=>{setIsClick(false)}}>선택 완료</BtnComplete>                
-                </S.MyPagePopUpBodyWrapper>
-                }
-
-        </S.MyPagePopUpWrapper>
-    )
+  return (
+    <S.MyPagePopUpWrapper>
+      <S.TextWrapper>일기 작성 스타일</S.TextWrapper>
+      {isClick === false ? (
+        <S.MyPagePopUpBodyWrapper>
+          <BtnCurDiaryType
+            onClick={() => {
+              setIsClick(true);
+            }}
+            curType={curType}
+          />
+          <BtnSetCharacter
+            onClick={() => {
+              handlegoWiki();
+            }}
+          />
+          <BtnLogout
+            onClick={() => {
+              handleLogout();
+            }}
+          />
+        </S.MyPagePopUpBodyWrapper>
+      ) : (
+        <S.MyPagePopUpBodyWrapper>
+          <SetWritingStyle transparency='100' />
+          <BtnComplete
+            onClick={() => {
+              setIsClick(false);
+            }}
+          >
+            선택 완료
+          </BtnComplete>
+        </S.MyPagePopUpBodyWrapper>
+      )}
+    </S.MyPagePopUpWrapper>
+  );
 }
