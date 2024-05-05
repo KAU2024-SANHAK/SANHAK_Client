a5f3b0c7cbdca9e8db021a730e16166581fb1bc5
P1su <pissssssu@naver.com>
2024-05-05 22:25:55 +0900

    feat: dairyType api 연결 구현

--- src/components/SetDiaryStyle/SetDiaryStyle.jsx
+++ src/components/SetDiaryStyle/SetDiaryStyle.jsx
@@ -1,31 +1,40 @@
 import * as S from './SetDiaryStyle.style'
 import { useRecoilState } from 'recoil'
 import { UserDiaryType } from '../../recoil/atoms'
+import { usePostDiaryType } from '../../hooks/queries/onboarding/usePostDiaryType'
 import BtnSetDiaryType from '../common/buttons/setDiaryType/BtnSetDiaryType'
 
-
-const handleSetWritingStyle= ()=>{
-    console.log('api 요청하세요')
-}
-
 export default function SetDiaryStyle({transparency}){
-    const [diaryType,setDiaryType] = useRecoilState(UserDiaryType)
+    const [diaryType,setDiaryType] = useRecoilState(UserDiaryType);
+    const { mutation } = usePostDiaryType();
+
+    const handleSetWritingStyle= ()=>{
+        const body = {
+            userDiaryType : diaryType,
+        };
+
+        mutation.mutate(body, {
+            onSucess: (data) => {
+                console.log(data);
+            }
+        })
+    };
 
     return(
         <S.Wrapper $transparency={transparency}>
 
                 <BtnSetDiaryType isClick = {diaryType === 'FASTTYPE' ? true : false}
                     onClick={()=>{
-                    handleSetWritingStyle();
-                    setDiaryType('FASTTYPE')
+                        setDiaryType('FASTTYPE');
+                        handleSetWritingStyle();
                     }} >
                     빠른 일기
                 </BtnSetDiaryType>
 
                 <BtnSetDiaryType isClick = {diaryType === 'SLOWTYPE' ? true : false}
                     onClick={()=>{
-                    handleSetWritingStyle();
-                    setDiaryType('SLOWTYPE')
+                        setDiaryType('SLOWTYPE');
+                        handleSetWritingStyle();
                     }} >
                     느린 일기
                 </BtnSetDiaryType>
