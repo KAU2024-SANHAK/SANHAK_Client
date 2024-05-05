aa7c9e6a0900c65dac1a7da3ad08a0a693223def
funcsom <bishop@kau.kr>
2024-05-05 11:02:23 +0900

    STYLE: 컴포넌트 이름 수정

--- src/pages/SearchByEmotion/SearchByEmotionPopUp/SearchByEmotionPopUp.jsx
+++ src/pages/SearchByEmotion/SearchByEmotionPopUp/SearchByEmotionPopUp.jsx
@@ -1,20 +1,20 @@
 import * as S from './SearchByEmotionPopUp.style'
-import EmotionType from '../../../components/common/buttons/EmotionType/EmotionType'
+import BtnEmotionType from '../../../components/common/buttons/EmotionType/EmotionType'
 
 
 export default function SearchByEmotionPopUp(){
     return(
         <S.Wrapper>
             <S.SearchByEmotionPopUpWrapper1>
-                <EmotionType name='기쁨' btnEnum="HAPPY"/>
-                <EmotionType name='분노' btnEnum="ANGRY"/>
-                <EmotionType name='평온' btnEnum="RELAX"/>
+                <BtnEmotionType name='기쁨' btnEnum="HAPPY"/>
+                <BtnEmotionType name='분노' btnEnum="ANGRY"/>
+                <BtnEmotionType name='평온' btnEnum="RELAX"/>
             </S.SearchByEmotionPopUpWrapper1>
 
             <S.SearchByEmotionPopUpWrapper2>
-                <EmotionType name='슬픔' btnEnum="SAD"/>
-                <EmotionType name='걱정' btnEnum="WORRIED"/>
-                <EmotionType name='놀람' btnEnum="SURPRISED"/>
+                <BtnEmotionType name='슬픔' btnEnum="SAD"/>
+                <BtnEmotionType name='걱정' btnEnum="WORRIED"/>
+                <BtnEmotionType name='놀람' btnEnum="SURPRISED"/>
             </S.SearchByEmotionPopUpWrapper2>
         </S.Wrapper>
         
