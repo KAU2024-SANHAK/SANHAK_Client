da13034ca59b911893e433b11b25b0a9b03ef844
funcsom <bishop@kau.kr>
2024-05-05 19:52:29 +0900

    FEAT: Calendar 컴포넌트 구현

--- src/components/common/Calendar/Calendar.jsx
+++ src/components/common/Calendar/Calendar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css'; 
-import { getMonth } from "date-fns";
+import { getDate, getMonth } from "date-fns";
 import { getYear } from "date-fns";
 import * as S from './Calendar.style'
+import BtnNextMonth from '../buttons/Next/BtnNextMonth'
+import BtnPrevMonth from '../buttons/Prev/BtnPrevMonth'
 
 const range = (start, end) => {
   return Array.from({ length: end - start + 1 }, (_, index) => start + index);
@@ -13,81 +13,75 @@ const ReactDatePicker = () => {
   const [startDate, setStartDate] = useState(new Date());
   const years = range(2023, getYear(new Date()), 1);
   const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "5월",
+    "6월",
+    "7월",
+    "8월",
+    "9월",
+    "10월",
+    "11월",
+    "12월",
   ];
   
 	return (
-		<S.CalendarWrapper>
-      <S.CalendarComponentWrapper>
-        <DatePicker
-          renderCustomHeader={({
-            date,
-            changeYear,
-            changeMonth,
-            decreaseMonth,
-            increaseMonth,
-            prevMonthButtonDisabled,
-            nextMonthButtonDisabled,
-          }) => (
-            <div
-              style={{
-                margin: 16,
-                display: "flex",
-                justifyContent: "center",
-              }}
+    <S.CalendarComponentWrapper>
+      <S.CustomDatePicker
+        formatWeekDay={(nameOfDay) => nameOfDay.substring(0, 1)}
+        selected={startDate}
+        useWeekdaysShort={false}
+        onChange={(date) => setStartDate(date)}
+        calendarClassName="oneStripes"
+        maxDate={new Date()}
+        inline
+
+        renderCustomHeader={({
+          date,
+          changeYear,
+          changeMonth,
+          decreaseMonth,
+          increaseMonth,
+          prevMonthButtonDisabled,
+          nextMonthButtonDisabled,
+        }) => (
+          <S.HeaderWrapper>
+            <S.BtnLeftWrapper onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+              <BtnPrevMonth/>
+            </S.BtnLeftWrapper>
+            <S.YearSelector
+              value={getYear(date)}
+              onChange={({ target: { value } }) => changeYear(value)}
             >
-              <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
-                {"이전달"}
-              </button>
-              <select
-                value={getYear(date)}
-                onChange={({ target: { value } }) => changeYear(value)}
-              >
-                {years.map((option) => (
-                  <option key={option} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
-    
-              <select
-                value={months[getMonth(date)]}
-                onChange={({ target: { value } }) =>
-                  changeMonth(months.indexOf(value))
-                }
-              >
-                {months.map((option) => (
-                  <option key={option} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
-    
-              <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
-                {"다음달"}
-              </button>
-            </div>
-          )}
-          selected={startDate}
-          useWeekdaysShort={false}
-          onChange={(date) => setStartDate(date)}
-          maxDate={new Date()}
-          inline
-          className="custom-datepicker" // 추가: 클래스 이름을 지정하여 스타일 적용
-        />
-      </S.CalendarComponentWrapper>
-		</S.CalendarWrapper>
+              {years.map((option) => (
+                <S.YearOption key={option} value={option}>
+                  {option}
+                </S.YearOption>
+              ))}
+            </S.YearSelector>
+  
+            <S.MonthSelector
+              value={months[getMonth(date)]}
+              onChange={({ target: { value } }) =>
+                changeMonth(months.indexOf(value))
+              }
+            >
+              {months.map((option) => (
+                <S.MonthOption key={option} value={option}>
+                  {option}
+                </S.MonthOption>
+              ))}
+            </S.MonthSelector>
+  
+            <S.BtnRightWrapper onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+              <BtnNextMonth/>
+            </S.BtnRightWrapper>
+          </S.HeaderWrapper>
+        )}
+      />
+    </S.CalendarComponentWrapper>
   );
 };
 
