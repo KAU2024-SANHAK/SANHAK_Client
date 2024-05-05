f8300f87aad20ccdd7679369f9e35bcc069f4ce8
funcsom <bishop@kau.kr>
2024-05-05 11:02:03 +0900

    STYLE: 컴포넌트 이름 수정 및 스타일 단위 수정 (px -> rem)

--- src/components/common/buttons/EmotionType/EmotionType.jsx
+++ src/components/common/buttons/EmotionType/EmotionType.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 // API 통신을 위한 import
 // import { usePostFeelingList } from "../../../../hooks/queries/mypage/usePostFeelingList";
 
-export default function EmotionType({name, btnEnum}) {
+export default function BtnEmotionType({name, btnEnum}) {
     const navigate=useNavigate();
     const handleClick = () => {
         // 백에서 API 가져와서 Feeling btnEnum 넘기고 해당 Feeling에 해당하는 일기를 불러오는 페이지로 들어가기
