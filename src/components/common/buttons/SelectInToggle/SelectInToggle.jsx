e6a4e08bf661aa3f1415176d764fcfec5ad811ed
funcsom <bishop@kau.kr>
2024-05-05 21:49:00 +0900

    FEAT: 달력 내 토글올리기 버튼 구현

---
+++ src/components/common/buttons/SelectInToggle/SelectInToggle.jsx
@@ -0,0 +1,15 @@
+import * as S from './SelectInToggle.style'
+export default function SelectInToggle({currentYear, currentMonth}) {
+
+    return (
+        //  클릭되지 않으면 none, 클릭되면 popup, popup 안에 어떤 컴포넌트가 들어갈지도 정하기
+        <S.Wrapper>
+            {currentYear}.
+            {currentMonth + 1}
+            <S.CalendarToggleWrapper>
+                <S.CalendarToggle/>
+            </S.CalendarToggleWrapper>
+        </S.Wrapper>
+
+    )
+}
