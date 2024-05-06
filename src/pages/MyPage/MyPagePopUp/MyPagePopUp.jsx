4a50818951f837083f16e01954be8ba12ebf70e5
P1su <pissssssu@naver.com>
2024-05-06 16:05:23 +0900

    feat: myPage에서 diaryType api 연결

--- src/pages/MyPage/MyPagePopUp/MyPagePopUp.jsx
+++ src/pages/MyPage/MyPagePopUp/MyPagePopUp.jsx
@@ -1,41 +1,78 @@
-import * as S from './MyPagePopUp.style'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useRecoilValue } from 'recoil'
-import { userDiaryType } from '../../../recoil/atoms'
-import useLogout from '../../../hooks/queries/member/members/useLogout'
-import BtnLogout from '../../../components/common/buttons/Logout/BtnLogout'
-import BtnComplete from '../../../components/common/buttons/complete/BtnComplete'
-import BtnSetCharacter from '../../../components/common/buttons/SetCharcater/BtnSetCharacter'
-import BtnCurDiaryType from '../../../components/common/buttons/CurDiaryType/BtnCurDiaryType'
-import SetWritingStyle from '../../../components/SetDiaryStyle/SetDiaryStyle'
-
+import * as S from './MyPagePopUp.style';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { userDiaryType } from '../../../recoil/atoms';
+import { usePostDiaryType } from "../../../hooks/queries/onboarding/usePostDiaryType";
+import useLogout from '../../../hooks/queries/member/members/useLogout';
+import BtnLogout from '../../../components/common/buttons/Logout/BtnLogout';
+import BtnComplete from '../../../components/common/buttons/complete/BtnComplete';
+import BtnSetCharacter from '../../../components/common/buttons/SetCharcater/BtnSetCharacter';
+import BtnCurDiaryType from '../../../components/common/buttons/CurDiaryType/BtnCurDiaryType';
+import SetWritingStyle from '../../../components/SetDiaryStyle/SetDiaryStyle';
 
 export default function MyPagePopUp(){
-    const [isClick, setIsClick] = useState(false);
-    const curType = useRecoilValue(userDiaryType)
+  const [isClick, setIsClick] = useState(false);
+  const { mutation } = usePostDiaryType();
+  const curType = useRecoilValue(userDiaryType)
+  const diaryTypeAtom = useRecoilValue(userDiaryType);
 
-    const navigate = useNavigate();
-    const handlegoWiki=()=>{
-        navigate('/mypage/characterwiki')
-    }
-    return(
-        <S.MyPagePopUpWrapper>
+  const handlePostDiaryStyle = () => {
+    const body = {
+      userDiaryType : diaryTypeAtom,
+    };
 
-            <S.TextWrapper>일기 작성 스타일</S.TextWrapper>
-                {isClick === false ? 
-                <S.MyPagePopUpBodyWrapper>
-                    <BtnCurDiaryType onClick={()=>{setIsClick(true)}} curType={curType}/>
-                    <BtnSetCharacter onClick={()=>{handlegoWiki()}}/>
-                    <BtnLogout onClick={()=>{useLogout()}}/>
-                </S.MyPagePopUpBodyWrapper>
-                :
-                <S.MyPagePopUpBodyWrapper>
-                    <SetWritingStyle transparency='100'/>
-                    <BtnComplete onClick={()=>{setIsClick(false)}}>선택 완료</BtnComplete>                
-                </S.MyPagePopUpBodyWrapper>
-                }
+    mutation.mutate(body, {
+        onSucess: (data) => {
+            console.log(data);
+        }
+    })
+  };
 
-        </S.MyPagePopUpWrapper>
-    )
+  const navigate = useNavigate();
+  const handlegoWiki = () => {
+    navigate('/mypage/characterwiki');
+  }
+  return(
+    <S.MyPagePopUpWrapper>
+
+      <S.TextWrapper>일기 작성 스타일</S.TextWrapper>
+
+        {isClick === false ? 
+          <S.MyPagePopUpBodyWrapper>
+            <BtnCurDiaryType 
+              onClick = {() => {
+                  setIsClick(true);
+              }} 
+              curType = {curType}
+            />
+            <BtnSetCharacter 
+              onClick = {() => {
+                  handlegoWiki();
+              }}
+            />
+            <BtnLogout 
+              onClick = {() => { 
+                  useLogout()
+              }}
+            />
+          </S.MyPagePopUpBodyWrapper>
+        :
+        <S.MyPagePopUpBodyWrapper>
+            <SetWritingStyle 
+              transparency = '100'
+            />
+            <BtnComplete 
+              onClick={() => {
+                handlePostDiaryStyle();
+                setIsClick(false);
+              }}
+            >
+              선택 완료
+            </BtnComplete>                
+        </S.MyPagePopUpBodyWrapper>
+        }
+
+    </S.MyPagePopUpWrapper>
+  )
 }
