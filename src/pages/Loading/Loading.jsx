366e17d59d49bb76dfd1c736db83ec6a7c259022
P1su <pissssssu@naver.com>
2024-05-06 18:23:57 +0900

    feat: Loading 페이지에 Lottie 적용

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -1,8 +1,13 @@
 import * as S from './Loading.style';
+import Lottie from 'lottie-react';
+import loadingLottie from '../../assets/lottie/loadingLottie.json'
 
 const Loading = () => {
   return (
     <S.LoadingWrapper>
+      <Lottie 
+        animationData = {loadingLottie}
+      />
       <S.Text>불러오는 중...</S.Text>
     </S.LoadingWrapper>
   );
