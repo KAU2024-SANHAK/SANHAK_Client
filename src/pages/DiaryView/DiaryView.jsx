5c21909a8fe525fc964e7c7c00969620c029d2f3
funcsom <bishop@kau.kr>
2024-05-06 15:04:43 +0900

    STYLE: 브랜치 이동을 위해 임시 저장합니다!

--- src/pages/DiaryView/DiaryView.jsx
+++ src/pages/DiaryView/DiaryView.jsx
@@ -12,29 +12,30 @@ import { usePostAdvice } from '../../hooks/queries/create/usePostAdvice';
 
 // utils에서 테스트용 js 파일을 가져오는 코드
 import { diaryContent } from '../../utils/diaryContent';
+import { useNavigate } from 'react-router-dom';
 
 export default function DiaryView() {
 
     const [isClick, setIsClick] = useState(false);
-    const [showResponseBtn, setShowResponseBtn] = useState(false);
-    const [adviceState, setAdviceState] = useRecoilState(advice);
+    // const [showResponseBtn, setShowResponseBtn] = useState(false);
+    // const [adviceState, setAdviceState] = useRecoilState(advice);
 
-    const responseObject = {diaryContent}
+    // const responseObject = {diaryContent}
 
     // data에 advice가 존재하는 지 여부 확인하기
-    const checkAdviceExists = () => {
-        return !!adviceState;
-    }
+    // const checkAdviceExists = () => {
+    //     return !!adviceState;
+    // }
 
-    const setDiaryAdvice = (adv) => {
-        advice.set(adv);
-    }
+    // const setDiaryAdvice = (adv) => {
+    //     advice.set(adv);
+    // }
 
     // advice가 있는지 확인하여 useEffect로 렌더링
-    useEffect(() => {
-        const adviceExists = checkAdviceExists();
-        setShowResponseBtn(adviceExists);
-    }, []);
+    // useEffect(() => {
+    //     const adviceExists = checkAdviceExists();
+    //     setShowResponseBtn(adviceExists);
+    // }, []);
 
     const requestPopUpToggle = () => {
         // API 연결 시 활성화할 코드
@@ -55,12 +56,13 @@ export default function DiaryView() {
                     title={diaryContent.diaryTitle}    
                 />
                 <S.GoToReplyBtnWrapper>
-                    {/* 리코일에 조언이 저장되어 있다면 Response 버튼을 렌더링하고 그렇지 않으면 Request 버튼을 렌더링 */}
+                    {/* 리코일에 조언이 저장되어 있다면 Response 버튼을 렌더링하고 그렇지 않으면 Request 버튼을 렌더링
                     {showResponseBtn ? (
                         <ResponseReplyViewBtn onClick={responsePopUpToggle}/>
                     ) : (
                         <RequestReplyViewBtn onClick={requestPopUpToggle}/>
-                    )}
+                    )} */}
+                    <ResponseReplyViewBtn onClick={() => {responsePopUpToggle()}}/>
                 </S.GoToReplyBtnWrapper>
 
                 {/* isClick의 상태에 따라 PopUp 렌더링 여부 결정 */}
--- src/utils/diaryContent.js
+++ src/utils/diaryContent.js
@@ -1,17 +1,24 @@
 export const diaryContent = [
     {
-        "status": 201,
-        "message": "요청이 성공했습니다.",
-        "data":{
-            "diaryId":"1",
-            "diary_content":"string",
-            "advice":{
-                "spicy": "string",
-                "kind": "string",
-            },
-            "feeling": "HAPPY",
-            "imageUrl": "fsdfsfdsfdsf",
-            "diaryTitle":"오늘나의하루"
-        }
-    }
+        "diaryId":"1",
+        "diary_content":"string",
+        "advice":{
+            "spicy": "string",
+            "kind": "string",
+        },
+        "feeling": "HAPPY",
+        "imageUrl": "fsdfsfdsfdsf",
+        "diaryTitle":"오늘나의하루"
+    },
+    {
+        "diaryId":"1",
+        "diary_content":"string",
+        "advice":{
+            "spicy": "string",
+            "kind": "string",
+        },
+        "feeling": "HAPPY",
+        "imageUrl": "fsdfsfdsfdsf",
+        "diaryTitle":"오늘나의하루"
+}
 ];
