03c92f8d8b5e8acd3f8fa0737e2ee52fd53c77a0
Wang HoEun <hoeunwang@gmail.com>
2024-05-08 20:34:42 +0900

    refactor: 비동기적 처리로 수정

--- src/hooks/queries/fastdiary/usePostKeywords.jsx
+++ src/hooks/queries/fastdiary/usePostKeywords.jsx
@@ -1,19 +1,19 @@
-import { aiInstance } from "../../../apis/client";
-import { useMutation } from "react-query";
-import axios from "axios";
+import { aiInstance } from '../../../apis/client';
+import { useMutation } from 'react-query';
+import axios from 'axios';
 
 export const postKeywords = async (body) => {
-    const response = aiInstance.post('/api/ai/diary/create', body);
-    return response.data;
-}
+  const response = await aiInstance.post('/api/ai/diary/create', body);
+  return response.data;
+};
 
 //diaryKeywords로 recoil 정보 넘겨줌
 export const usePostKeywords = () => {
-    const mutation  = useMutation({
-        mutationFn: postKeywords,
-        onSuccess: (data) => {
-            console.log('요청에 성공했습니다.', data)
-        }
-    })
-    return { mutation };
-}
+  const mutation = useMutation({
+    mutationFn: postKeywords,
+    onSuccess: (data) => {
+      console.log('요청에 성공했습니다.', data);
+    },
+  });
+  return { mutation };
+};
