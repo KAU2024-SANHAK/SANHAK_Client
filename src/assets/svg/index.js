86eeaffc931158ddba7b4da9cbd54144a93e308d
funcsom <bishop@kau.kr>
2024-05-08 21:59:03 +0900

    STYLE: index에서 안쓰는 이미지 찾아서 삭제

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -2,7 +2,6 @@ export { default as IcError } from './IcError';
 
 export { default as IcMyPage} from './IcMyPage';
 export { default as IcSearchByEmotion} from './IcSearchByEmotionButton';
-export { default as IcThisMonthEmotion} from './IcWriteDiaryButton';
 export { default as IcCurrentProgressCircle } from './IcCurrentProgressCircle'
 export { default as IcHoneyBear } from './IcHoneyBear' 
 export { default as IcKakaoLogin } from './IcKakaoLogin' 
@@ -29,3 +28,4 @@ export { default as IcAddImage } from './IcAddImage'
 export { default as IcPrevMonthBtn } from './IcPrevMonthBtn'
 export { default as IcNextMonthBtn } from './IcNextMonthBtn'
 export { default as IcCalendarToggle } from './IcCalendarToggle'
+export { default as IcWriteDiaryButton } from './IcWriteDiaryButton'
