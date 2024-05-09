9844d2b7fbcf24148eedbd200dee7ec6d774e6a8
P1su <pissssssu@naver.com>
2024-05-09 12:06:07 +0900

    feat: 느린 일기 값 atom 저장 후 DiaryView 페이지에서 바로 출력되도록 수정

--- src/pages/MyPage/MyPagePopUp/MyPagePopUp.jsx
+++ src/pages/MyPage/MyPagePopUp/MyPagePopUp.jsx
@@ -12,29 +12,74 @@ import BtnCurDiaryType from '../../../components/common/buttons/CurDiaryType/Btn
 import SetWritingStyle from '../../../components/SetDiaryStyle/SetDiaryStyle';
 
 export default function MyPagePopUp(){
-    const [isClick, setIsClick] = useState(false);
-    const curType = useRecoilValue(userDiaryType)
+  const [isClick, setIsClick] = useState(false);
+  const postDiaryTypeMutation = usePostDiaryType();
+  const logoutMutation = useLogout();
+  const navigate = useNavigate();
+  const curType = useRecoilValue(userDiaryType)
+  const diaryTypeAtom = useRecoilValue(userDiaryType);
 
-    const navigate = useNavigate();
-    const handlegoWiki=()=>{
-        navigate('/mypage/characterwiki')
-    }
-    return(
-        <S.MyPagePopUpWrapper>
+  const handlePostDiaryStyle = () => {
+    const body = {
+      userDiaryType : diaryTypeAtom,
+    };
 
-            <S.TextWrapper>일기 작성 스타일</S.TextWrapper>
-                {isClick === false ? 
-                <S.MyPagePopUpBodyWrapper>
-                    <BtnCurDiaryType onClick={()=>{setIsClick(true)}} curType={curType}/>
-                    <BtnSetCharacter onClick={()=>{handlegoWiki()}}/>
-                    <BtnLogout onClick={()=>{useLogout()}}/>
-                </S.MyPagePopUpBodyWrapper>
-                :
-                <S.MyPagePopUpBodyWrapper>
-                    <SetWritingStyle transparency='100'/>
-                    <BtnComplete onClick={()=>{setIsClick(false)}}>선택 완료</BtnComplete>                
-                </S.MyPagePopUpBodyWrapper>
-                }
+    postDiaryTypeMutation.mutate(body, {
+        onSucess: (data) => {
+            console.log(data);
+        }
+    })
+  };
+
+  const handleLogout = () => {
+    logoutMutation.mutate();
+    localStorage.removeItem('LOGIN_TOKEN');
+    localStorage.removeItem('LOGIN_REFRESH_TOKEN');
+    navigate('/');
+  }
+
+  const handlegoWiki = () => {
+    navigate('/mypage/characterwiki');
+  }
+  return(
+    <S.MyPagePopUpWrapper>
+
+      <S.TextWrapper>일기 작성 스타일</S.TextWrapper>
+
+        {isClick === false ? 
+          <S.MyPagePopUpBodyWrapper>
+            <BtnCurDiaryType 
+              onClick = {() => {
+                  setIsClick(true);
+              }} 
+              curType = {curType}
+            />
+            <BtnSetCharacter 
+              onClick = {() => {
+                  handlegoWiki();
+              }}
+            />
+            <BtnLogout 
+              onClick = {() => { 
+                  handleLogout()
+              }}
+            />
+          </S.MyPagePopUpBodyWrapper>
+        :
+        <S.MyPagePopUpBodyWrapper>
+            <SetWritingStyle 
+              transparency = '100'
+            />
+            <BtnComplete 
+              onClick={() => {
+                handlePostDiaryStyle();
+                setIsClick(false);
+              }}
+            >
+              선택 완료
+            </BtnComplete>                
+        </S.MyPagePopUpBodyWrapper>
+        }
 
     </S.MyPagePopUpWrapper>
   )
