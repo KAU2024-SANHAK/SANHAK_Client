8d6d73464a3637c8be645659d48aa47725c3608c
P1su <pissssssu@naver.com>
2024-05-09 22:22:54 +0900

    feat: reponse data를 console창에 입력

--- src/components/common/buttons/ThisMonthEmotion/ThisMonthEmotion.jsx
+++ src/components/common/buttons/ThisMonthEmotion/ThisMonthEmotion.jsx
@@ -3,17 +3,10 @@ import useGetSummary from "../../../../hooks/queries/main/useGetSummary";
 
 export default function ThisMonthEmotion() {
   const date = new Date();
-  const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
 
   const { data, isLoading, isError } = useGetSummary();
   console.log(data);
-/* const mutation = usePostSummary();
-  console.log(formattedDate);
-  mutation.mutate(formattedDate,{
-    onSuccess: (response) => {
-      console.log(response)
-    }
-  })*/
+  
   return (
     <S.Wrapper>
       <S.Boxe/>
