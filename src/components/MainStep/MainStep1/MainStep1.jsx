00decacc58bcba540decb5e948858fd38413f37e
P1su <pissssssu@naver.com>
2024-05-09 18:15:07 +0900

    add: tanstack/react-query 설치

--- src/components/MainStep/MainStep1/MainStep1.jsx
+++ src/components/MainStep/MainStep1/MainStep1.jsx
@@ -3,7 +3,6 @@ import MyPageButton from '../../common/buttons/MyPageButton/MyPageButton'
 import SearchByEmotionButton from '../../common/buttons/SearchByEmotionButton/SearchByEmotionButton'
 import HoneyBearMainStep1 from '../../common/HoneyBearMainStep1/HoneyBearMainStep1'
 import ThisMonthEmotion from '../../common/buttons/ThisMonthEmotion/ThisMonthEmotion'
-// import { HoneyBear } from '../../../pages/MyPage/MyPage.style'
 
 export default function MainStep1(){
     return(
@@ -11,11 +10,6 @@ export default function MainStep1(){
             <S.MainStep1HoneyBearWrapper>
               <HoneyBearMainStep1/>
             </S.MainStep1HoneyBearWrapper>
-
-            {/* honeyBear가 height=50이 가장 커진거라서...ㅜㅜ 재사용하기가 힘듭니다 */}
-            {/* <S.MainStep1HoneyBearWrapper>
-              <HoneyBear height='50rem' />
-            </S.MainStep1HoneyBearWrapper> */}
 
             <S.MainButtonWrapper>
                 <MyPageButton />
