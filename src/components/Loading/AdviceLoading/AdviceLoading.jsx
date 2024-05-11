37ad1c3f1be171c24bf7abfa7a3c134c363ec0a9
P1su <pissssssu@naver.com>
2024-05-12 00:55:47 +0900

    feat: Advice Lottie 컴포넌트 생성

---
+++ src/components/Loading/AdviceLoading/AdviceLoading.jsx
@@ -0,0 +1,16 @@
+import * as S from './AdviceLoading.style';
+import Lottie from 'lottie-react';
+import adviceLottie from '../../../assets/lottie/adviceLottie.json'
+
+export default function AdviceLoading(){
+  return (
+    <S.LoadingWrapper>
+      <S.LottieWrapper>
+      <Lottie 
+        animationData = {adviceLottie}
+      />
+      </S.LottieWrapper>
+      <S.Text>조언을 생성 중입니다</S.Text>
+    </S.LoadingWrapper>
+  );
+};
---
+++ src/components/Loading/AdviceLoading/AdviceLoading.style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const LoadingWrapper = styled.div`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  background: ${({ theme: { colors } }) => colors.pink.white_pink};
+  width: 100%;
+  height: 100%;
+`;
+
+export const Text = styled.div`
+  ${({ theme: { fonts } }) => fonts.body_10};
+  //color: ${({ theme: { colors } }) => colors.normal.black};
+
+`;
+
+export const LottieWrapper = styled.div`
+  width: 100%;
+`
