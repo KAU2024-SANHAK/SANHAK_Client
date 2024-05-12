5f855dc9e715b9e1f8988f99c7e91cfb483a23b1
funcsom <bishop@kau.kr>
2024-05-13 00:40:51 +0900

    감정별 일기조회 일기리스트 스타일 수정

--- src/pages/DiaryList/DiaryList.sytle.js
+++ src/pages/DiaryList/DiaryList.sytle.js
@@ -4,18 +4,14 @@ export const DiaryListWrapper=styled.div`
     ${({ theme: { mixin } }) => mixin.flexCenter({})};
     background-color: ${({ theme }) => theme.colors.pink.white_pink};
     width: 100%;
-    height: 100vh;
 `
 export const BackButtonWrapper = styled.div`
-    position: absolute;
-    top: 2rem;
-    left: 2.8rem;
+    ${({ theme: { mixin } }) => mixin.flexBox({})};
+    width: 32rem;
+    margin-top: 3.2rem;
 `
 export const DiaryListComponentWrapper = styled.div`
     ${({ theme: { mixin } }) => mixin.flexCenter({})};
-    position:absolute;
-    top: 14.5rem;
-    padding: 0rem 0rem 4.4rem 0rem;
-    background-color: ${({ theme }) => theme.colors.pink.white_pink};
-    width:100%
+    margin: 2.8rem 0 4.4rem 0;
+    gap: 0.8rem;
 `
