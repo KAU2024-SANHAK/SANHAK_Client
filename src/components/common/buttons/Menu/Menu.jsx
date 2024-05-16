65a7e494b549f1cb498a6fdf72a4b58f088bd5b3
funcsom <bishop@kau.kr>
2024-05-17 07:21:01 +0900

    FEAT: Menu 버튼 누르면 mutation 이전까지

--- src/components/common/buttons/Menu/Menu.jsx
+++ src/components/common/buttons/Menu/Menu.jsx
@@ -1,9 +1,39 @@
+import { useState } from 'react'
 import * as S from './Menu.style'
 
-export default function Menu({handleClick}){
+export default function Menu({id}){
+    const [isClicked, setIsClicked] = useState(false);
+    
+    const deleteDiary = () => {
+        
+        const body = {
+            diaryId: id,
+          };
+          mutation.mutate(body, {
+            onSuccess: (response) => {
+              console.log(response.message)
+            },
+          });
+    }
+
+    const editDiary = () => {
+        console.log('editing')
+    }
+
     return(
-        <S.ButtonWrapper onClick={()=>{handleClick()}}> 
-            <S.MenuBtn/>
+        <S.ButtonWrapper onClick={()=>{setIsClicked(!isClicked)}}> 
+            <S.MenuBtn />
+            { isClicked ?
+            <S.MenuWrapper>
+                <S.EditBtn onClick={() => {editDiary()}}>
+                    일기 수정하기
+                </S.EditBtn>
+                <S.DeleteBtn onClick={() => {deleteDiary()}}>
+                    일기 삭제하기
+                </S.DeleteBtn>
+            </S.MenuWrapper>
+            :
+            null}
         </S.ButtonWrapper>
     )
 }
--- src/components/common/buttons/Menu/Menu.style.js
+++ src/components/common/buttons/Menu/Menu.style.js
@@ -1,11 +1,31 @@
 import styled from "styled-components";
 import { IcMenu } from "../../../../assets/svg";
 
-export const ButtonWrapper =styled.button`
+export const ButtonWrapper =styled.div`
     display: flex;
     background-color: transparent;
     margin: 0 1.2rem 0 1.2rem;
 `
 export const MenuBtn = styled(IcMenu)`
     display: flex;
+`
+export const MenuWrapper = styled.div`
+    ${({ theme: { mixin } }) => mixin.flexCenter({})};
+    width: 13rem;
+    height: 8.5rem;
+    background-color: white;
+`
+export const EditBtn = styled.button`
+    ${({ theme }) => theme.fonts.body_02};
+    ${({ theme: { mixin } }) => mixin.flexCenter({})};
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    background-color: transparent;
+`
+export const DeleteBtn = styled.button`
+    ${({ theme }) => theme.fonts.body_02};
+    ${({ theme: { mixin } }) => mixin.flexCenter({})};
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    background-color: transparent;
 `
