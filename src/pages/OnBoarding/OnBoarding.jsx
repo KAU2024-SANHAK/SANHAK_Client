106e48f1674acabcdcc7e2790c85e28956dd1107
필 수 <104068583+P1su@users.noreply.github.com>
2024-05-17 18:27:03 +0900

    [리팩토링] 개발자 모드 없는 화면에서 스타일 깨지지 않도록 수정 및 첫 로그인 후 온보딩에서 api 되도록 수정 (#103)
    * style: 필요없는 컴포넌트와 svg 파일 삭제
    
    * style: 느린 일기 페이지 들여쓰기 수정
    
    * feat: useResetKeywords 에서 느린일기 값들도 삭제하도록 수정
    
    * style: 걱정과 평온 ENUM값 수정
    
    * styel: 온보딩 페이지 들여쓰기 수정
    
    * style: 온보딩 버튼 maxwidth로 수정
    
    * style: 빠른 일기 header에 position: relative 추가
    
    * style: 빠른 일기 HoneyBear 스타일 수정
    
    * style: 마이페이지 Style 반응형으로 수정
    
    * style: 감정 리스트 Style 반응형으로 수정
    
    * fix: usePostDiaryType 헤더에서 getAccessToken을 직접 호출

--- src/pages/OnBoarding/OnBoarding.jsx
+++ src/pages/OnBoarding/OnBoarding.jsx
@@ -1,4 +1,5 @@
 import * as S from './OnBoarding.style'
+import { useEffect } from 'react'
 import { useFunnel } from '../../hooks/common/useFunnel'
 import OnBoardingStep1 from '../../components/OnBoardingSteps/OnBoardingStep1'
 import OnBoardingStep2 from '../../components/OnBoardingSteps/OnBoardingStep2'
@@ -8,43 +9,51 @@ import OnBoardingStep5 from '../../components/OnBoardingSteps/OnBoardingStep5'
 import OnBoardingStep6 from '../../components/OnBoardingSteps/OnBoardingStep6'
 import { useNavigate } from 'react-router-dom'
 import StepProgress from '../../components/common/StepPrgoress/StepProgress'
+import { getAccessToken } from '../../apis/client'
 
 export default function OnBoarding(){
+  const navigate = useNavigate();
+  const steps = [
+    { name: 'Step1', component: OnBoardingStep1, nextStep: 'Step2' },
+    { name: 'Step2', component: OnBoardingStep2, nextStep: 'Step3' },
+    { name: 'Step3', component: OnBoardingStep3, nextStep: 'Step4' },
+    { name: 'Step4', component: OnBoardingStep4, nextStep: 'Step5' },
+    { name: 'Step5', component: OnBoardingStep5, nextStep: 'Step6' },
+    { name: 'Step6', component: OnBoardingStep6, nextStep: '/main' },
+  ];
+  const [Funnel, Step, currentStep, setCurrentStep] = useFunnel("Step1");
+  
+  const handleNext = () => {
+    const nextStepIndex = steps.findIndex(step => step.name === currentStep) + 1;
 
-    const navigate = useNavigate();
-    const steps = [
-        { name: 'Step1', component: OnBoardingStep1, nextStep: 'Step2' },
-        { name: 'Step2', component: OnBoardingStep2, nextStep: 'Step3' },
-        { name: 'Step3', component: OnBoardingStep3, nextStep: 'Step4' },
-        { name: 'Step4', component: OnBoardingStep4, nextStep: 'Step5' },
-        { name: 'Step5', component: OnBoardingStep5, nextStep: 'Step6' },
-        { name: 'Step6', component: OnBoardingStep6, nextStep: '/main' }
-    ]
-    const [Funnel, Step, currentStep, setCurrentStep] = useFunnel("Step1")
-    
-    const handleNext=()=>{
-        const nextStepIndex = steps.findIndex(step => step.name === currentStep) + 1;
-
-        if (nextStepIndex < steps.length) {
-            setCurrentStep(steps[nextStepIndex].name);
-        } else {
-            navigate(steps[nextStepIndex-1].nextStep);
-        }
+    if (nextStepIndex < steps.length) {
+      setCurrentStep(steps[nextStepIndex].name);
+    } else {
+      navigate(steps[nextStepIndex-1].nextStep);
     }
+  };
 
-    return(
-        <S.OnBoardingPageWrapper>
-            <Funnel>
-                {steps.map((step, idx)=>(
-                    <Step key = {idx} name = {step.name}>
-                        <S.ProgressWrapper>
-                            <StepProgress steps= {steps} cur={step.name}/>        
-                        </S.ProgressWrapper>
-                        <step.component onNext={()=>{handleNext()}}/>
-                    </Step>
-                ))}
+  return(
+    <S.OnBoardingPageWrapper>
+      <Funnel>
+        {steps.map((step, idx)=>(
+          <Step 
+            key={idx} 
+            name={step.name}
+          >
+            <S.ProgressWrapper>
+              <StepProgress 
+                steps={steps} 
+                cur={step.name}
+              />        
+            </S.ProgressWrapper>
+            <step.component onNext={() => {
+              handleNext();
+            }}/>
+          </Step>
+        ))}
 
-            </Funnel>
-        </S.OnBoardingPageWrapper>
-    )
+      </Funnel>
+    </S.OnBoardingPageWrapper>
+  )
 }
--- src/pages/SlowDiary/SlowDiary.jsx
+++ src/pages/SlowDiary/SlowDiary.jsx
@@ -11,121 +11,128 @@ import BtnNext from '../../components/common/buttons/Next/BtnNext';
 import HomeModal from '../../components/Modal/HomeModal';
 
 export default function SlowDiary(){
-    const date = new Date();
-    const today = date.getDate();
-    const formData = new FormData();
-    const navigate = useNavigate();
-    const [isOpen, openModal, closeModal] = useModal();
-    const mutation = usePostSlowDiary();
-    const readData = useFileReader();
-    const [image, setImage] = useRecoilState(diaryImage);
-    const [title, setTitle] = useRecoilState(diaryTitle);
-    const [content, setContent] = useRecoilState(diaryContent);
-    const [id, setId] = useRecoilState(diaryId);
+  const date = new Date();
+  const today = date.getDate();
 
-    const [data, setData] = useState({
-        imageurl: '',
-        diaryTitle: '',
-        diaryContent: ''
-    });
-    const [file, setFile] = useState('');
+  const formData = new FormData();
+  const navigate = useNavigate();
+  const [isOpen, openModal, closeModal] = useModal();
+  const mutation = usePostSlowDiary();
+  const readData = useFileReader();
+  const [image, setImage] = useRecoilState(diaryImage);
+  const [title, setTitle] = useRecoilState(diaryTitle);
+  const [content, setContent] = useRecoilState(diaryContent);
+  const [id, setId] = useRecoilState(diaryId);
+
+  const [data, setData] = useState({
+    imageurl: image,
+    diaryTitle: title,
+    diaryContent: content,
+  });
+
+  const [file, setFile] = useState('');
+  
+  const blobTitle = new Blob([JSON.stringify(data.diaryTitle)],{
+    type: 'application/json',
+  });        
+  const blobContent = new Blob([JSON.stringify(data.diaryContent)],{
+    type: 'application/json',
+  });
+  
+  const deleteQuotes = (text) => {
+    return text.substring(1, text.length-1);
+  };
+
+  const handleChange = (event) => {
+    setData({...data, [event.target.name] : event.target.value});
+  };
+
+  const handleImage = (event) => {
+    const file = event.target.files[0];
     
-    const blobTitle = new Blob([JSON.stringify(data.diaryTitle)],{
-        type: 'application/json',
-    });        
-    const blobContent = new Blob([JSON.stringify(data.diaryContent)],{
-        type: 'application/json',
+    readData(file, (imageFile) => {
+      setData({...data, [event.target.name]: imageFile});
+    })
+
+    setFile(file);
+  };
+
+  const handleSubmit = () => {
+    formData.append('imageurl', file);
+    formData.append('diaryTitle',blobTitle);
+    formData.append('diaryContent',blobContent);
+
+    mutation.mutate(formData, {
+      onSuccess: (response) => {
+        const data = response.data;
+        setImage(data.imageurl);
+        setContent(deleteQuotes(data.diaryContent));
+        setTitle(deleteQuotes(data.diaryTitle));
+        setId(data.diaryId);
+        navigate('/diaryview');
+      }
     });
-    
-    const deleteQuotes = (text) => {
-        return text.substring(1, text.length-1);
-    };
+      
+  };
 
-    const handleChange = (event) => {
-        setData({...data, [event.target.name] : event.target.value});
-    };
+  return(
+    <S.SlowDiaryPageWrapper  $isEven = {today%2}>
+      {isOpen && 
+        <HomeModal 
+          closeModal={closeModal} 
+        />  
+      }
 
-    const handleImage = (event) => {
-        const file = event.target.files[0];
-        
-        readData(file, (imageFile) => {
-            setData({...data, [event.target.name]: imageFile});
-        })
+      <S.SlowDiaryHeader>
+        <BtnHome
+          onClick={() => {
+            openModal()
+          }} 
+        />       
+        <S.Caption>
+          사진을 첨부하려면 + 버튼을 누르세요.
+        </S.Caption>
+      </S.SlowDiaryHeader>
 
-        setFile(file);
-    };
+      <S.FormWrapper onSubmit={handleSubmit}>
+        <S.ImageInput 
+          type = 'file' 
+          id ='imgInput' 
+          accept ='image/*' 
+          name = 'imageurl' 
+          onChange = {handleImage} />
 
-    const handleSubmit = () => {
-        formData.append('imageurl', file);
-        formData.append('diaryTitle',blobTitle);
-        formData.append('diaryContent',blobContent);
+        <S.Label htmlFor = 'imgInput'>
+          {data.imageurl ? (
+            <S.PreviewImg 
+              src = {data.imageurl} 
+            />
+          ): 
+            <S.AddImg />
+          } 
+        </S.Label>
 
-        mutation.mutate(formData, {
-            onSuccess: (response) => {
-                const data = response.data;
-                setImage(data.imageurl);
-                setContent(deleteQuotes(data.diaryContent));
-                setTitle(deleteQuotes(data.diaryTitle));
-                setId(data.diaryId);
-                navigate('/diaryview');
-            }
-        });
-        
-    };
+          <S.TitleInput 
+            type = 'text' 
+            placeholder = '제목을 입력해주세요.' 
+            name = 'diaryTitle' 
+            value = {data.diaryTitle} 
+            onChange = {handleChange}
+          />
+          <S.ContentInput 
+            type = 'text' 
+            placeholder = '오늘 무슨 일이 있었나요? 자유롭게 적어주세요.'
+            name = 'diaryContent' 
+            value = {data.diaryContent} 
+            onChange = {handleChange}
+          />
 
-    return(
-        <S.SlowDiaryPageWrapper  $isEven = {today%2}>
-            {isOpen && <HomeModal closeModal = {closeModal} />}
-
-            <S.SlowDiaryHeader>
-                <BtnHome
-                    onClick={() => {
-                        openModal()
-                    }} 
-                />       
-                <S.Caption>
-                    사진을 첨부하려면 + 버튼을 누르세요.
-                </S.Caption>
-            </S.SlowDiaryHeader>
-
-            <S.FormWrapper onSubmit={handleSubmit}>
-                <S.ImageInput 
-                    type = 'file' 
-                    id ='imgInput' 
-                    accept ='image/*' 
-                    name = 'imageurl' 
-                    onChange = {handleImage} />
-                <S.Label htmlFor = 'imgInput'>
-                    {data.imageurl ? (
-                        <S.PreviewImg 
-                            src = {data.imageurl} 
-                        />
-                    ): 
-                        <S.AddImg />
-                    } 
-                </S.Label>
-
-                <S.TitleInput 
-                    type = 'text' 
-                    placeholder = '제목을 입력해주세요.' 
-                    name = 'diaryTitle' 
-                    value = {data.diaryTitle} 
-                    onChange = {handleChange}
-                />
-                <S.ContentInput 
-                    type = 'text' 
-                    placeholder = '오늘 무슨 일이 있었나요? 자유롭게 적어주세요.'
-                    name = 'diaryContent' 
-                    value = {data.diaryContent} 
-                    onChange = {handleChange}
-                />
-
-                <S.BtnField>
-                    <BtnNext onNext={() => {
-                        handleSubmit();
-                    }}/>
-                </S.BtnField>
-            </S.FormWrapper>
-        </S.SlowDiaryPageWrapper>
-    )   
+          <S.BtnField>
+            <BtnNext onNext={() => {
+              handleSubmit();
+            }}/>
+          </S.BtnField>
+      </S.FormWrapper>
+    </S.SlowDiaryPageWrapper>
+  )   
 }
