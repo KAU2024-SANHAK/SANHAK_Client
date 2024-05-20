e640cc87df489b12016d0da32180cad8d200acb7
funcsom <bishop@kau.kr>
2024-05-21 01:57:14 +0900

    style: main step1에서의 스타일 수정

--- src/components/MainStep/MainStep1/MainStep1.style.js
+++ src/components/MainStep/MainStep1/MainStep1.style.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
-export const MainStep1Wrapper = styled.div`
-    ${({ theme: { mixin } }) => mixin.flexCenter({})};
+export const Main1Wrapper = styled.div`
+    ${({ theme: { mixin } }) => mixin.flexBox({direction: "column", justify: 'end'})};
     width: 100%;
     height: 100vh;
+`
+export const MainStep1Wrapper = styled.div`
+    ${({ theme: { mixin } }) => mixin.flexCenter({})};
+    gap: 1.5rem;
+    margin-bottom: 3.7rem;
 `
 export const MainButtonWrapper = styled.div`
     ${({ theme: { mixin } }) => mixin.flexBox({direction:'column'})};
