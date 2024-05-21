6966ff43a15463608e3699f4ee7ebe73b06b8ddd
Wang HoEun <hoeunwang@gmail.com>
2024-05-21 19:42:43 +0900

    refactor: main2 페이지 속 허니베어 wrapper에 height 지정

--- src/components/MainStep/MainStep2/MainStep2.style.js
+++ src/components/MainStep/MainStep2/MainStep2.style.js
@@ -1,35 +1,35 @@
 import styled from 'styled-components';
 
 export const Main2Wrapper = styled.div`
-    ${({ theme: { mixin } }) => mixin.flexBox({direction: "column"})};
-    width: 100%;
-    min-height: 100vh;
-    height: auto;
-    max-height: fit-content;
+  ${({ theme: { mixin } }) => mixin.flexBox({ direction: 'column' })};
+  width: 100%;
+  min-height: 100vh;
+  height: auto;
+  max-height: fit-content;
 `;
 export const MainStep2Wrapper = styled.div`
-    ${({ theme: { mixin } }) => mixin.flexCenter({})};
-`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+`;
 export const MainStep2ComponentWrapper = styled.div`
-    ${({ theme: { mixin } }) => mixin.flexCenter({})};
-    width: 32rem;
-    margin: 19.8rem 0 10.9rem 0;
-    gap: 0.8rem;
-`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  width: 32rem;
+  margin: 19.8rem 0 10.9rem 0;
+  gap: 0.8rem;
+`;
 export const MainStep2HoneyBearWrapper = styled.div`
-    position: absolute;
-    right: 9rem;
-    top: 4rem;
-`
+  position: absolute;
+  height: 100vh;
+  right: 9rem;
+  top: 4rem;
+`;
 export const NoDiaryExist = styled.div`
-    ${({ theme: { mixin } }) => mixin.flexCenter({})};
-    width: 32rem;
-    height: 9.3rem;
-    flex-shrink: 0;
-    border-radius: 3rem;
-    background: rgba(255, 255, 255, 0.27);
-    backdrop-filter: blur(5px);
-    color: #575757;
-    ${({ theme }) => theme.fonts.body_08}
-
-`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  width: 32rem;
+  height: 9.3rem;
+  flex-shrink: 0;
+  border-radius: 3rem;
+  background: rgba(255, 255, 255, 0.27);
+  backdrop-filter: blur(5px);
+  color: #575757;
+  ${({ theme }) => theme.fonts.body_08}
+`;
