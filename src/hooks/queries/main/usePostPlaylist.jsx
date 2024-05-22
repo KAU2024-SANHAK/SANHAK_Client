d2284d179e42eaa511fda4461924915b81d0007d
P1su <pissssssu@naver.com>
2024-05-22 18:07:55 +0900

    feat: usePostPlaylist hook 생성

---
+++ src/hooks/queries/main/usePostPlaylist.jsx
@@ -0,0 +1,19 @@
+import { aiInstance } from '../../../apis/client';
+import { useMutation } from '@tanstack/react-query';
+
+export const postPlaylist = async () => {
+  const response = await aiInstance.post('api/ai/diary/youtube',body);
+  return response;
+}
+
+const usePostPlaylist = () => {
+  const mutation = useMutation({
+    mutationFn: postPlaylist,
+    onSuccess: (data) => {
+      console.log('요청에 성공했습니다.', data);
+    }
+  });
+
+  return mutation;
+}
+export default usePostPlaylist;
