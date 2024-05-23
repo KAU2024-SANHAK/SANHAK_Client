2fb94c795e0840b3afb958f174253fd55f597578
P1su <pissssssu@naver.com>
2024-05-24 00:26:37 +0900

    feat: api 연결

--- src/pages/SharedView/SharedView.jsx
+++ src/pages/SharedView/SharedView.jsx
@@ -1,9 +1,12 @@
+import useGetSharedDiary from '../../hooks/queries/etc/useGetSharedDiary';
 import * as S from './SharedView.style'
 import { useParams } from 'react-router-dom'
 
 export default function SharedView(){
   const { id } = useParams();
+  const { data } = useGetSharedDiary(id);
   console.log(id);
+  console.log(data)
 
   return(
     <S.SharedViewPageWrapper>
