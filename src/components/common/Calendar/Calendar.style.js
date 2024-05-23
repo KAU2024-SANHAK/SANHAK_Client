64d7550987b1430fb8b8e14bac0f11095f025370
필 수 <104068583+P1su@users.noreply.github.com>
2024-05-23 21:55:47 +0900

    refactor: 모든 기종 블러처리 코드 추가 (#141)

--- src/components/common/Calendar/Calendar.style.js
+++ src/components/common/Calendar/Calendar.style.js
@@ -12,6 +12,10 @@ export const CalendarComponentWrapper = styled.div`
         width: 32rem;
         border-radius: 3rem;
         background: rgba(255, 255, 255, 0.7);
+        -webkit-backdrop-filter: blur(1.5rem);
+        -moz-backdrop-filter: blur(1.5rem);
+        -o-backdrop-filter: blur(1.5rem);
+        -ms-backdrop-filter: blur(1.5rem);
         backdrop-filter: blur(1.5rem);
         border: none;
     }
