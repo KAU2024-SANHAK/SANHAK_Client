9ce0f6ee6f4f2ef36cc15b3b9553f8d11c748c79
Wang HoEun <hoeunwang@gmail.com>
2024-05-24 11:01:17 +0900

    refactor: 온보딩 100vh로 수정

--- src/components/OnBoardingSteps/OnBoardingStep.style.js
+++ src/components/OnBoardingSteps/OnBoardingStep.style.js
@@ -1,47 +1,45 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const OnBoardingStepWrapper = styled.div`
-    ${({ theme: { mixin } }) => mixin.flexCenter({})};
-    width: 100%;
-    height: 100%;
-`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  width: 100%;
+  height: 100vh;
+`;
 export const OnBoardingHeader = styled.header`
-    ${({ theme: { mixin } }) => mixin.flexBox({justify:'center',align: 'center' })};
-    position: absolute;
-    top: 5rem;
-    width: 37.5rem;
-`
+  ${({ theme: { mixin } }) => mixin.flexBox({ justify: 'center', align: 'center' })};
+  position: absolute;
+  top: 5rem;
+  width: 37.5rem;
+`;
 export const OnBoardingBody = styled.div`
-    ${({ theme: { mixin } }) => mixin.flexBox({direction:'column', align: 'center' })};
-    width: 100%;
-    text-align: left;
-    height: 64%;
-    padding-top: 6.5rem;
-`
+  ${({ theme: { mixin } }) => mixin.flexBox({ direction: 'column', align: 'center' })};
+  width: 100%;
+  text-align: left;
+  height: 64%;
+  padding-top: 6.5rem;
+`;
 export const OnBoardingFooter = styled.footer`
-    ${({ theme: { mixin } }) => mixin.flexCenter({})};
-    position: fixed;
-    bottom: 1rem;
-    margin-top: 10rem;
-`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  position: fixed;
+  bottom: 1rem;
+  margin-top: 10rem;
+`;
 export const OnBoardingTitleWrapper = styled.div`
-    ${({ theme: { mixin } }) => mixin.flexCenter({ })};
-    position: absolute;
-    top: 6.5rem;
-    width: 100%;
-`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  position: absolute;
+  top: 6.5rem;
+  width: 100%;
+`;
 export const OnBoardingImgWrapper = styled.div`
-    height: 35.3%;
-`
-export const OnBoardingImg = styled.img`
-
-`
+  height: 35.3%;
+`;
+export const OnBoardingImg = styled.img``;
 export const OnBoardingHoneyBearWrapper = styled.div`
-    margin-left: 1rem;
-    height: 57%;
-`
+  margin-left: 1rem;
+  height: 57%;
+`;
 
 export const ButtonField = styled.div`
-    position: absolute;
-    right: 2rem;
-`
+  position: absolute;
+  right: 2rem;
+`;
