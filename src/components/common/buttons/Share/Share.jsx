3f091a168b229a44017234fdec30e3eb4ab7ceb0
P1su <pissssssu@naver.com>
2024-05-25 22:03:34 +0900

    feat: 기본 이미지 추가

--- src/components/common/buttons/Share/Share.jsx
+++ src/components/common/buttons/Share/Share.jsx
@@ -1,14 +1,19 @@
-import * as S from './Share.style'
-import { shareKakao } from '../../../../utils/shareKakao'
+import * as S from './Share.style';
+import { shareKakao } from '../../../../utils/shareKakao';
+import sampleImage from '../../../../assets/img/dayHoneyBear.webp'
 
 export default function BtnShare({title, image, id}){
+	const handleShare = () => {
+		// if(image === null){
+		// 	image = sampleImage;
+		// }
+		console.log(image)
+		shareKakao(title, image, id)
+	};
 
-    const handleShare = () => {
-        shareKakao(title, image, id)
-    }
-    return(
-        <S.ButtonWrapper onClick={()=>{handleShare()}}> 
-            <S.ShareBtn/>
-        </S.ButtonWrapper>
-    )
+	return(
+		<S.ButtonWrapper onClick={()=>{handleShare()}}> 
+			<S.ShareBtn/>
+		</S.ButtonWrapper>
+	)
 }
