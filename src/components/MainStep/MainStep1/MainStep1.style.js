b113e0121e120c6e3d07941e98c90cde4e0a6acf
Wang HoEun <hoeunwang@gmail.com>
2024-05-29 01:00:56 +0900

    refactor: main 페이지 버튼 wrapper flex space-between 사용해서 긴 핸드폰에서도 깨지지 않도록 하기

--- src/components/MainStep/MainStep1/MainStep1.style.js
+++ src/components/MainStep/MainStep1/MainStep1.style.js
@@ -3,16 +3,18 @@ import styled from 'styled-components';
 export const Main1Wrapper = styled.div`
   ${({ theme: { mixin } }) => mixin.flexCenter({})};
   width: 100%;
+  height: 100vh;
 `;
 export const MainStep1Wrapper = styled.div`
   ${({ theme: { mixin } }) => mixin.flexCenter({})};
   gap: 1.5rem;
   margin-bottom: 3.7rem;
   z-index: 10;
+  height: 100%;
 `;
 export const MainButtonWrapper = styled.div`
-  ${({ theme: { mixin } }) => mixin.flexBox({ direction: 'column' })};
+  ${({ theme: { mixin } }) => mixin.flexBox({ direction: 'column', justify: 'space-between' })};
+  height: 100vh;
   margin-top: 8.7rem;
   margin-right: 11rem;
-  gap: 1.5rem;
 `;
