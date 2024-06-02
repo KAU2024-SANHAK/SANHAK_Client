52d32eab62aa0e96fb683a4cc399d6441b8e2a43
필 수 <104068583+P1su@users.noreply.github.com>
2024-06-02 18:32:13 +0900

    [refactor] 마이페이지, 감정별 조회 페이지, 느린 일기, 빠른 일기 페이지 스타일 수정  (#168)
    * style: 느린 일기 기본 이미지 삭제
    
    * style: 추천 플리 p 태그에 cursor: pointer 적용
    
    * style: 감정별 조회 백버튼 웹 화면 밖으로 안 튀어나가도록 수정
    
    * style: 들여쓰기 수정
    
    * style: 마이페이지 top wrapper width값 수정하여 백 버튼 위치 조정
    
    * style: 감정별 조회 백버튼 wrapper 삭제 후 top wrapper로만 위치 조정되도록 수정
    
    * style: slowDiary 헤더 width값 조정
    
    * refactor: slowDiary 하단 버튼 위치 조정
    
    * refactor: Diaryview 페이지 상단 버튼 width 조정하여 웹 화면 넘어가지 않도록 수정
    
    * refactor: 빠른일기 페이지 컴포넌트 재배치
    
    * refactor: 빠른 일기 작성칸 height 설정
    
    * refactor: 빠른 일기 작성칸 width  설정
    
    * refactor: 빠른 일기 질문창과 답변창 중앙 정렬
    
    * refactor: 빠른 일기 작성 최상위 컴포넌트 height 수정
    
    * refactor: 빠른 일기 하단 버튼 필드 스타일 수정
    
    * refactor: 빠른 일기 Step1 버튼 필드 스타일 수정
    
    * refactor: HoneyPot padding 값 조절해서 중앙 정렬
    
    * refactor: 느린일기 페이지 컴포넌트 height값 수정

--- src/components/DiaryProgress/DiaryProgress.style.js
+++ src/components/DiaryProgress/DiaryProgress.style.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-    margin-right: 3rem;
 `
--- src/components/FastDiarySteps/Questions/LargeQustion.jsx
+++ src/components/FastDiarySteps/Questions/LargeQustion.jsx
@@ -2,10 +2,8 @@ import * as S from './Question.style';
 
 export default function LargeQuestion({ children }) {
   return (
-    <S.LargeQuestionWrapper>
       <S.LargeQuestionPng>
         <S.QuestionText>{children}</S.QuestionText>
       </S.LargeQuestionPng>
-    </S.LargeQuestionWrapper>
   );
 }
