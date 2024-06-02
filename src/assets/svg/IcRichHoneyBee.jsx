d2cfb52140fd6ab83ded85323e34cb4dabd25b12
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2024-06-02 21:13:45 +0900

    [feat] mypage character (#174)
    * feat: 새로운 허니베어 캐릭터 삽입 후 적용
    
    * refactor: import 문 한줄로 수정
    
    * feat: 꿀벌 캐릭터 추가 및 적용
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

---
+++ src/assets/svg/IcRichHoneyBee.jsx
@@ -0,0 +1,140 @@
+const SvgIcRichHoneyBee = () => (
+  <svg viewBox='0 0 359 269' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <ellipse
+      cx='143.453'
+      cy='47.8419'
+      rx='28.6908'
+      ry='41.8408'
+      transform='rotate(-15 143.453 47.8419)'
+      fill='#B4F2FF'
+    />
+    <ellipse
+      cx='196.053'
+      cy='47.8409'
+      rx='28.6908'
+      ry='41.8408'
+      transform='rotate(15 196.053 47.8409)'
+      fill='#B4F2FF'
+    />
+    <rect y='60.9917' width='358.637' height='208.009' rx='104.005' fill='#FCD44C' />
+    <path d='M95.6357 60.9922H150.627V269.001H95.6357V60.9922Z' fill='#2C2A28' />
+    <path d='M205.618 60.9922H260.609V269.001H205.618V60.9922Z' fill='#2C2A28' />
+    <ellipse cx='309.621' cy='139.89' rx='15.5409' ry='14.3454' fill='#2C2A28' />
+    <circle cx='306.036' cy='132.72' r='4.78181' fill='white' />
+    <rect
+      x='298.734'
+      y='27.2402'
+      width='10.0418'
+      height='43.5143'
+      transform='rotate(12.5215 298.734 27.2402)'
+      fill='#2C2A28'
+    />
+    <circle
+      cx='303.824'
+      cy='27.4282'
+      r='8.36816'
+      transform='rotate(8.16777 303.824 27.4282)'
+      fill='#2C2A28'
+    />
+    <rect
+      x='261.71'
+      y='28.9526'
+      width='10.202'
+      height='44.2087'
+      transform='rotate(-10.6463 261.71 28.9526)'
+      fill='#2C2A28'
+    />
+    <circle
+      cx='266.24'
+      cy='25.9779'
+      r='8.50166'
+      transform='rotate(-15 266.24 25.9779)'
+      fill='#2C2A28'
+    />
+    <path d='M344.827 138H278L292.429 166.067H331.917L344.827 138Z' fill='black' stroke='black' />
+    <rect x='286.688' y='144.683' width='4.00962' height='4.00962' fill='#D9D9D9' />
+    <rect x='290.697' y='148.692' width='5.34615' height='5.34615' fill='#D9D9D9' />
+    <rect x='296.043' y='154.038' width='4.00962' height='4.00962' fill='#D9D9D9' />
+    <rect x='324' y='135' width='35' height='6' fill='black' />
+    <path
+      d='M279.032 262.043C269.335 255.283 270.814 231.046 276.566 226.76C282.318 222.473 305.162 238.796 305.162 238.796H315.187C315.187 238.796 336.058 221.154 343.946 225.77C351.835 230.387 349.698 255.943 342.96 261.219C336.222 266.495 315.187 249.348 315.187 249.348H305.162C305.162 249.348 288.728 268.803 279.032 262.043Z'
+      fill='#D91A1A'
+    />
+    <path
+      d='M303.208 241.34L289.065 235.699'
+      stroke='#9E0F0F'
+      stroke-opacity='0.7'
+      stroke-width='5'
+      stroke-linecap='round'
+    />
+    <path
+      d='M330.645 252.187L316.502 246.545'
+      stroke='#9E0F0F'
+      stroke-opacity='0.7'
+      stroke-width='5'
+      stroke-linecap='round'
+    />
+    <path
+      d='M303.743 244.002L288.564 243.247'
+      stroke='#9E0F0F'
+      stroke-opacity='0.7'
+      stroke-width='5'
+      stroke-linecap='round'
+    />
+    <path
+      d='M333.333 244.265L318.135 244.304'
+      stroke='#9E0F0F'
+      stroke-opacity='0.7'
+      stroke-width='5'
+      stroke-linecap='round'
+    />
+    <path
+      d='M303.449 246.875L289.878 251.956'
+      stroke='#9E0F0F'
+      stroke-opacity='0.7'
+      stroke-width='5'
+      stroke-linecap='round'
+    />
+    <path
+      d='M331.716 235.663L318.145 240.744'
+      stroke='#9E0F0F'
+      stroke-opacity='0.7'
+      stroke-width='5'
+      stroke-linecap='round'
+    />
+    <g filter='url(#filter0_d_1590_246)'>
+      <rect x='303.354' y='236.322' width='14.7907' height='15.1686' rx='7.39536' fill='#D91A1A' />
+    </g>
+    <defs>
+      <filter
+        id='filter0_d_1590_246'
+        x='299.354'
+        y='236.322'
+        width='22.7905'
+        height='23.1685'
+        filterUnits='userSpaceOnUse'
+        color-interpolation-filters='sRGB'
+      >
+        <feFlood flood-opacity='0' result='BackgroundImageFix' />
+        <feColorMatrix
+          in='SourceAlpha'
+          type='matrix'
+          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+          result='hardAlpha'
+        />
+        <feOffset dy='4' />
+        <feGaussianBlur stdDeviation='2' />
+        <feComposite in2='hardAlpha' operator='out' />
+        <feColorMatrix type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0' />
+        <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_1590_246' />
+        <feBlend
+          mode='normal'
+          in='SourceGraphic'
+          in2='effect1_dropShadow_1590_246'
+          result='shape'
+        />
+      </filter>
+    </defs>
+  </svg>
+);
+export default SvgIcRichHoneyBee;
