282cc895ea44a0d4dd290be9beb3755bc38f8690
P1su <pissssssu@naver.com>
2024-06-03 16:10:04 +0900

    feat: 썸네일 클릭시 타이틀이 뜨며 타이틀 클릭시 유튜브 링크로 이동

--- src/components/MainStep/MainStep1/WeatherPlaylist/WeatherPlaylist.style.js
+++ src/components/MainStep/MainStep1/WeatherPlaylist/WeatherPlaylist.style.js
@@ -1,16 +1,25 @@
 import styled from "styled-components";
 
 export const WeatherPlaylistWrapper = styled.div`
-  
+  width: 100%;
 `;
 export const WeatherIcon = styled.img`
   width: 3rem;
   height: 3rem;
+  margin-left: 0.3rem;
+  margin-bottom: 0.4rem;
+`;
+export const PlaylistBox = styled.div`
+  ${({ theme: { mixin } }) => mixin.flexBox({align: 'center'})};
+  gap: 0.5rem;
 `;
 export const Thumbnail = styled.img`
   width: 3.6rem;
   height: 3.6rem;
   border-radius: 70%;
-  overflow: hidden;
-  background: black;
+  cursor: pointer;  
+`;
+export const PlaylistTitle = styled.p`
+  ${({ theme }) => theme.fonts.caption_01};
+  cursor: pointer;
 `;
