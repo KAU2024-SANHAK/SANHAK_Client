313e86ef314a24af9451101225a6319c804d15f1
P1su <pissssssu@naver.com>
2024-06-04 12:56:43 +0900

    refactor: 라우터를 생성하여 날씨 api 적용해두기

---
+++ src/pages/Weather/Weather.jsx
@@ -0,0 +1,11 @@
+import * as S from './Weather.style'
+import WeatherPlaylist from "../../components/MainStep/MainStep1/WeatherPlaylist/WeatherPlaylist";
+
+function Weather(){
+  return(
+    <S.Wrapper>
+      <WeatherPlaylist />
+    </S.Wrapper>
+  );
+}
+export default Weather
