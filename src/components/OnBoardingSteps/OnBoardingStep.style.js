255a68c9350e2c77b60b41f016fc27df42b8751c
필 수 <104068583+P1su@users.noreply.github.com>
2024-06-16 21:26:48 +0900

    refactor: step5 width및 margin 값 조정 (#191)

--- src/components/OnBoardingSteps/OnBoardingStep.style.js
+++ src/components/OnBoardingSteps/OnBoardingStep.style.js
@@ -13,13 +13,14 @@ export const OnBoardingHeader = styled.header`
 `;
 export const OnBoardingBody = styled.div`
   ${({ theme: { mixin } }) => mixin.flexBox({ direction: 'column', align: 'center' })};
-  width: 100%;
+  width: 32rem;
   text-align: left;
   height: 64%;
   padding-top: 6.5rem;
 `;
 export const OnBoardingFooter = styled.footer`
   ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  width: 32rem;
   position: fixed;
   bottom: 1rem;
   margin-top: 10rem;
