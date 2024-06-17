7f9e9787fd36f3b52736594d796f35560744cd4f
Wang HoEun <hoeunwang@gmail.com>
2024-06-17 14:03:45 +0900

    fix: 일기 수정하기 삭제하기 폰트 사이즈 줄이기

--- src/components/common/buttons/Menu/Menu.style.js
+++ src/components/common/buttons/Menu/Menu.style.js
@@ -1,40 +1,40 @@
-import styled from "styled-components";
-import { IcMenu } from "../../../../assets/svg";
+import styled from 'styled-components';
+import { IcMenu } from '../../../../assets/svg';
 
-export const ButtonWrapper =styled.div`
-    ${({ theme: { mixin } }) => mixin.flexBox({justify: 'end'})};
-    display: flex;
-    background-color: transparent;
-    margin: 0 1.2rem 0 1.2rem;
-`
+export const ButtonWrapper = styled.div`
+  ${({ theme: { mixin } }) => mixin.flexBox({ justify: 'end' })};
+  display: flex;
+  background-color: transparent;
+  margin: 0 1.2rem 0 1.2rem;
+`;
 export const MenuBtn = styled(IcMenu)`
-    display: flex;
-`
+  display: flex;
+`;
 export const MenuWrapper = styled.div`
-    ${({ theme: { mixin } }) => mixin.flexCenter({})};
-    height: 8.5rem;
-    background-color: white;
-    border-radius: 3rem;
-    padding: 0 1.7rem 0 1.7rem;
-    position: absolute;
-    top: 10rem;
-`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  height: 8.5rem;
+  background-color: white;
+  border-radius: 3rem;
+  padding: 0 1.7rem 0 1.7rem;
+  position: absolute;
+  top: 10rem;
+`;
 export const EditBtn = styled.button`
-    ${({ theme }) => theme.fonts.body_02};
-    ${({ theme: { mixin } }) => mixin.flexCenter({})};
-    margin-top: 0.5rem;
-    margin-bottom: 0.5rem;
-    background-color: transparent;
-    color: black;
-`
+  ${({ theme }) => theme.fonts.body_08};
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+  background-color: transparent;
+  color: black;
+`;
 export const DeleteBtn = styled.button`
-    ${({ theme }) => theme.fonts.body_02};
-    ${({ theme: { mixin } }) => mixin.flexCenter({})};
-    margin-top: 0.5rem;
-    margin-bottom: 0.5rem;
-    background-color: transparent;
-    color: black;
-`
+  ${({ theme }) => theme.fonts.body_08};
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+  background-color: transparent;
+  color: black;
+`;
 export const ButtonField = styled.div`
-    width: 37.5rem;
-`
+  width: 37.5rem;
+`;
