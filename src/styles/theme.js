3f90b3e6ef20f36205dc1b4085f01aaa3979ca22
Wang HoEun <hoeunwang@gmail.com>
2024-06-17 13:58:29 +0900

    fix: 모바일 숫자가 폰트 적용 안되는 이슈로 default 폰트 코드 삭제

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -43,28 +43,28 @@ const colors = {
 
 const fonts = {
   big_title: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 3.2rem;
     font-style: normal;
     font-weight: 700;
     line-height: 2.2rem;
   `,
   title: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 2.6rem;
     font-style: normal;
     font-weight: 700;
     line-height: 3rem;
   `,
   heading_01: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 2.4rem;
     font-style: normal;
     font-weight: 600;
     line-height: 3rem;
   `,
   heading_03: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 2.1rem;
     font-style: normal;
     font-weight: 700;
@@ -72,35 +72,35 @@ const fonts = {
   `,
 
   body_01: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 1.8rem;
     font-style: normal;
     font-weight: 700;
     line-height: 2.2rem;
   `,
   body_02: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 1.8rem;
     font-style: normal;
     font-weight: 400;
     line-height: 2.2rem;
   `,
   body_06: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 1.6rem;
     font-style: normal;
     font-weight: 500;
     line-height: 2.2rem;
   `,
   body_08: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 1.5rem;
     font-style: normal;
     font-weight: 400;
     line-height: 2.2rem;
   `,
   body_10: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 1.2rem;
     font-style: normal;
     font-weight: 400;
@@ -108,21 +108,21 @@ const fonts = {
   `,
 
   caption_01: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 1.2rem;
     font-style: normal;
     font-weight: 700;
     line-height: 1.6rem;
   `,
   caption_02: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 1.2rem;
     font-style: normal;
     font-weight: 400;
     line-height: 1.6rem;
   `,
   caption_04: css`
-    font-family: 'Pretendard', serif, sans-serif, monospace;
+    font-family: 'Pretendard';
     font-size: 1rem;
     font-style: normal;
     font-weight: 400;
