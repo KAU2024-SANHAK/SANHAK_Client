f2050d9511c2152fedbc66ca96f2fa47e5b5ff08
P1su <pissssssu@naver.com>
2024-07-15 00:35:35 +0900

    style: 배경색 theme 활용하여 적용

--- src/pages/SlowDiary/SlowDiary.style.js
+++ src/pages/SlowDiary/SlowDiary.style.js
@@ -5,8 +5,8 @@ export const SlowDiaryPageWrapper = styled.div`
   ${({ theme: { mixin } }) => mixin.flexCenter({})};
   width: 100%;
   min-height: 100vh;
-  background: ${(props) => (props.$isEven === 0 ? 'linear-gradient(187deg, #FFBBCB 20.43%, #FFDAE3 81.25%, #DCC6CC 100.45%, #999 100.45%);'
-  :'linear-gradient(187deg, #FFE768 20.43%, #FFF3B7 84.05%);'
+  background: ${(props) => (props.$isEven === 0 ? ({ theme }) => theme.colors.gradient.gradient_pink
+  :({ theme }) => theme.colors.gradient.gradient_yellow
   )};
 
 `
