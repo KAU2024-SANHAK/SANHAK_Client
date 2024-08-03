850e4cca9d48c198be6c6ad43695b0333ca8451b
P1su <pissssssu@naver.com>
2024-08-04 00:05:34 +0900

    style: px값을 rem으로 수정

--- src/components/common/buttons/Next/BtnNext.style.js
+++ src/components/common/buttons/Next/BtnNext.style.js
@@ -4,7 +4,7 @@ import { IcNextButton } from '../../../../assets/svg';
 export const ButtonWrapper = styled.button`
   display: flex;
   background-color: transparent;
-  border-radius: 50px;
+  border-radius: 5rem;
   box-shadow: -0.4rem 0.4rem 0.6rem  rgba(0, 0, 0, 0.3);
   padding: 0;
 `;
