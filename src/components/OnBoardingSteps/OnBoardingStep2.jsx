d6f8ab0af938204a525844fd3099544966fc358d
필 수 <104068583+P1su@users.noreply.github.com>
2024-08-25 00:26:42 +0900

    Refactor/#223 onboarding funnel refactoring (#224)
    * style: 온보딩 페이지 스타일 코드 수정
    
    * style: 온보딩 페이지 필요없는 import문 삭제
    
    * style: onboarding steps 스타일 코드, 들여쓰기, imort문 정리
    
    * feat: 온보딩 이미지 파일을 webp로 변경
    
    * feat: 다음 단계로 이동하는 ButtonField를 하단으로 수정
    
    * feat: step5 complete 버튼 props 넘기는 방식 수정 및 complete 버튼 이벤트 핸들러를 화살표 함수로 정의
    
    * feat: step6 complete 버튼 props 넘기는 방식 수정
    
    * style: OnBoardingHeader 컴포넌트 mixin을 flexCenter로 변경
    
    * style: 필요없는 스타일 컴포넌트 삭제
    
    * style: Next 버튼 코드를 가독성 좋게 수정
    
    * feat: onboarding 페이지에서 onNext props 넘겨주는 방식 수정
    
    * style: useFunnel의 Step과 Funnel 컴포넌트에서 props를 구조 분해 할당으로 작성
    
    * style: StepProgress 가독성 있게 수정
    
    * feat: useFunnel 에서 childStep에서 props 작성하는 방식 수정
    
    * style: Next 버튼에 그림자 추가
    
    * style: px값을 rem으로 수정

--- src/components/OnBoardingSteps/OnBoardingStep2.jsx
+++ src/components/OnBoardingSteps/OnBoardingStep2.jsx
@@ -1,35 +1,31 @@
-import * as S from "./OnBoardingStep.style"
-import BtnNext from "../common/buttons/Next/BtnNext"
-import Title from "./title/Title";
-import SubTitle from "./title/SubTitle";
-import onBoardingStep2Img from '../../assets/img/onBoardingStep2.png'
+import * as S from './OnBoardingStep.style';
+import BtnNext from '../common/buttons/Next/BtnNext';
+import Title from './title/Title';
+import SubTitle from './title/SubTitle';
+import onboardingStep2Webp from '../../assets/img/onboardingStep2.webp';
 
-export default function OnBoardingStep2({onNext}){
-    return (
-        <S.OnBoardingStepWrapper>
+export default function OnBoardingStep2({ onNext }){
+  return (
+    <S.OnBoardingStepWrapper>
+      <S.OnBoardingTitleWrapper>
+        <Title>
+          바쁘고 귀찮아...<br/>
+          도대체 일기는 언제 쓰지?               
+        </Title>
+        <SubTitle>
+          분주한 일상 속, <br/>
+          허니어리에 일기를 채워보세요
+        </SubTitle>
+      </S.OnBoardingTitleWrapper>
 
-            <S.OnBoardingHeader>
-                <S.ButtonField>
-                    <BtnNext onNext={()=>{onNext()}}/>
-                </S.ButtonField>
-            </S.OnBoardingHeader>
+      <S.OnBoardingImgWrapper>
+          <S.OnBoardingImg src={onboardingStep2Webp}/>
+      </S.OnBoardingImgWrapper>
 
-            <S.OnBoardingTitleWrapper>
-            <Title>
-                바쁘고 귀찮아...<br/>
-                도대체 일기는 언제 쓰지?               
-            </Title>
-            <SubTitle>
-                    분주한 일상 속, <br/>
-                    허니어리에 일기를 채워보세요
-            </SubTitle>
-            </S.OnBoardingTitleWrapper>
+      <S.ButtonField>
+        <BtnNext onNext={onNext}/>
+      </S.ButtonField>
+    </S.OnBoardingStepWrapper>
 
-            <S.OnBoardingImgWrapper>
-                <S.OnBoardingImg src={onBoardingStep2Img}/>
-            </S.OnBoardingImgWrapper>
-
-
-        </S.OnBoardingStepWrapper>
-    )
+  );
 }
--- src/components/common/buttons/Next/BtnNext.style.js
+++ src/components/common/buttons/Next/BtnNext.style.js
@@ -1,10 +1,13 @@
-import styled from 'styled-components'
-import { IcNextButton } from '../../../../assets/svg'
+import styled from 'styled-components';
+import { IcNextButton } from '../../../../assets/svg';
 
 export const ButtonWrapper = styled.button`
-    display: flex;
-    background-color: transparent;
-`
+  display: flex;
+  background-color: transparent;
+  border-radius: 5rem;
+  box-shadow: -0.4rem 0.4rem 0.6rem  rgba(0, 0, 0, 0.3);
+  padding: 0;
+`;
 export const NextButton = styled(IcNextButton)`
-    display: flex;
-`
+  display: flex;
+`;
