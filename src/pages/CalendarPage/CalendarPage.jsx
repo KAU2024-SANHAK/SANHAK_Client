ac21bd937ef1df33bac4613ccc862cc010330a6f
P1su <pissssssu@naver.com>
2024-08-28 12:53:09 +0900

    feat: 달력 컴포넌트와 로직 코드를 달력 페이지에 적용

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,9 +1,35 @@
 import * as S from './CalendarPage.style';
+import { clickedDiary } from '../../recoil/atoms';
+import { useRecoilValue } from 'recoil';
+import { format } from 'date-fns';
+import Calendar from '../../components/common/Calendar/Calendar';
+import DiaryListComponent from '../../components/common/DiaryListComponent/DiaryListComponent';
+
 
 function CalendarPage() {
+  const diaries = useRecoilValue(clickedDiary);
+
   return(
     <S.CalendarPageWrapper>
-      달력 페이지입니다.
+      <Calendar />
+      {diaries.length > 0 
+        ? (
+            diaries.map(item => (
+              <DiaryListComponent
+                key={item.diaryId}
+                feelingListId={item.diaryId}
+                feelingListTitle={item.diaryTitle}
+                feelingListDate={format(new Date(item.createdDate), "yyyy년 M월 d일")}
+                imageUrl={item.imageUrl}
+              />
+            ))
+          ) 
+        : (
+            <S.ErrorText>
+              아직 작성된 일기가 없습니다.
+            </S.ErrorText>
+          )
+      }
     </S.CalendarPageWrapper>
   );
 }
--- src/pages/CalendarPage/CalendarPage.style.js
+++ src/pages/CalendarPage/CalendarPage.style.js
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
 
 export const CalendarPageWrapper = styled.div`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  background: ${({ theme }) => theme.colors.gradient.gradient_pink};
   width: 100%;
+  min-height: 100vh;
+`;
+export const ErrorText = styled.div`
+  ${({ theme: { mixin } }) => mixin.flexCenter({})};
+  width: 32rem;
+  height: 9.3rem;
+  flex-shrink: 0;
+  border-radius: 3rem;
+  background: rgba(255, 255, 255, 0.27);
+  -webkit-backdrop-filter: blur(1.5rem);
+  -moz-backdrop-filter: blur(1.5rem);
+  -o-backdrop-filter: blur(1.5rem);
+  -ms-backdrop-filter: blur(1.5rem);
+  backdrop-filter: blur(1.5rem);
+  color: #575757;
+  ${({ theme }) => theme.fonts.body_08}
 `;
