df419269ed2565791b930a8cc36110033dea93ae
P1su <pissssssu@naver.com>
2024-09-03 01:59:45 +0900

    style: 메인페이지에서 사용하지 않는 라이브러리와 스타일 코드 삭제

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,27 +1,8 @@
 import * as S from './Main.style';
 import MainStep1 from '../../components/MainStep/MainStep1/MainStep1';
-import React, { useState } from 'react';
-import WriteDiaryButton from '../../components/common/buttons/WriteDiaryButton/WriteDiaryButton';
-import BtnSlowDiary from '../../components/common/buttons/DiaryType/BtnSlowDiary';
-import BtnFastDiary from './../../components/common/buttons/DiaryType/BtnFastDiary';
-import { useNavigate } from 'react-router-dom';
 import CreateDiaryButtonField from '../../components/CreateDiaryButtonField/CreateDiaryButtonField';
 
 function Main() {
-  const [isClick, setIsClick] = useState(false);
-  const navigate = useNavigate();
-
-  const onUpdate = () => {
-    setIsClick(!isClick);
-  };
-
-  const handleFastDiary = () => {
-    navigate('/fastdiary');
-  };
-  const handleSlowDiary = () => {
-    navigate('/slowdiary');
-  };
-
   return (
     <S.MainWrapper>
       <S.HoneyBearWrapper>
