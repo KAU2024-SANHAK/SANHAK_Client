8059a0141454ab3e5bf2041e0ddaeb79a649c660
P1su <pissssssu@naver.com>
2024-09-03 01:44:16 +0900

    style: 사용하지 않는 변수 삭제

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,35 +1,14 @@
 import * as S from './Main.style';
 import MainStep1 from '../../components/MainStep/MainStep1/MainStep1';
-import MainStep2 from '../../components/MainStep/MainStep2/MainStep2';
 import React, { useState } from 'react';
-import Slider from 'react-slick';
 import WriteDiaryButton from '../../components/common/buttons/WriteDiaryButton/WriteDiaryButton';
-import StepProgress from '../../components/common/StepPrgoress/StepProgress';
-import { motion } from 'framer-motion';
 import BtnSlowDiary from '../../components/common/buttons/DiaryType/BtnSlowDiary';
 import BtnFastDiary from './../../components/common/buttons/DiaryType/BtnFastDiary';
 import { useNavigate } from 'react-router-dom';
 
 function Main() {
-  const [oldSlide, setOldSlide] = useState(0);
-  const [activeSlide, setActiveSlide] = useState(0);
   const [isClick, setIsClick] = useState(false);
   const navigate = useNavigate();
-  const settings = {
-    dots: false,
-    infinite: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    adaptiveHeight: true,
-    beforeChange: (current, next) => {
-      setOldSlide(current);
-      setActiveSlide(next);
-    },
-  };
-  const steps = [
-    { name: 'MainStep1', component: <MainStep1 /> },
-    { name: 'MainStep2', component: <MainStep2 /> },
-  ];
 
   const onUpdate = () => {
     setIsClick(!isClick);
