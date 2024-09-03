b279a2a7c0ddd7e44d168f52ce35581ec07a12be
P1su <pissssssu@naver.com>
2024-09-03 23:33:55 +0900

    style: MenuHeader width값과 padding 값 조정

--- src/components/MenuHeader/MenuHeader.style.js
+++ src/components/MenuHeader/MenuHeader.style.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export const MenuHeaderWrapper = styled.div`
   ${({ theme: { mixin } }) => mixin.flexBox({ 'justify' : 'flex-end' })};
   background-color: ${({ theme }) => theme.colors.pink.background_pink};
-  width: 100%;
+  width: 32rem;
   position: fixed;
-  padding: 2rem 2rem 1rem 0;
+  padding: 2rem 0 1rem 0;
   top: 0;
   gap: 2.5rem;
   z-index: 10;
